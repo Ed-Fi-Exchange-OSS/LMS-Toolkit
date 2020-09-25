dabf8ca5c19445b71e214ac91fb38503858a630e
Stephen A. Fuqua <stephen@safnet.com>
2020-09-25 13:45:41 -0500

    Staging users table

--- src/lms-ds-installer/mssql/0001-table-user.sql
+++ src/lms-ds-installer/mssql/0001-table-user.sql
@@ -25,3 +25,29 @@ CREATE TABLE lms.[User] (
         SourceSystem
     )
 ) ON [PRIMARY];
+
+CREATE TABLE lms.[stg_User] (
+    StagingId INT NOT NULL IDENTITY,
+    SourceSystemIdentifier VARCHAR(50) NOT NULL,
+    SourceSystem VARCHAR(50) NOT NULL,
+    UserRole VARCHAR(50) NOT NULL,
+    LocalUserIdentifier VARCHAR(50),
+    SISUserIdentifier VARCHAR(50),
+    [Name] VARCHAR(250),
+    EmailAddress VARCHAR(320),
+    EntityStatus VARCHAR(50),
+    CreateDate DATETIME2,
+    LastModifiedDate DATETIME2,
+    CONSTRAINT PK_stg_User PRIMARY KEY CLUSTERED (
+        StagingId ASC
+    ) WITH(
+        PAD_INDEX = OFF,
+        STATISTICS_NORECOMPUTE = OFF,
+        IGNORE_DUP_KEY = OFF,
+        ALLOW_ROW_LOCKS = ON,
+        ALLOW_PAGE_LOCKS = ON
+    ) ON [PRIMARY],
+) ON [PRIMARY];
+
+CREATE INDEX IX_stg_User_Natural_Key ON lms.[stg_user] (SourceSystemIdentifier, SourceSystem, LastModifiedDate);
+
