9be03009728912ec4e62d3c01741d4c81457dddc
Stephen A. Fuqua <stephen@safnet.com>
2020-09-25 09:08:44 -0500

    Inject file headers

--- experimental/schoology/main.py
+++ experimental/schoology/main.py
@@ -1,3 +1,8 @@
+﻿# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
 """
 This script retrieves and writes out to CSV all students, sections, assignments,
 and submissions for the desired grading period(s). See readme.md for more info.
