03fa748979a7f5dce0ca626b1d44f38d0c1a7a90
Luis Richmond <lalrg@outlook.com>
2020-10-22 08:18:18 -0600

    Update tests to include new map method in facade

--- src/schoology-extractor/tests/test_schoology_extract_facade.py
+++ src/schoology-extractor/tests/test_schoology_extract_facade.py
@@ -254,9 +254,8 @@ def describe_when_getting_assignments():
 
             # Act
             result = service.get_assignments(section_id)
-            mapped_result = service.map_assignments_to_udm(result, section_id)
 
-            return mapped_result, get_assignments_mock
+            return result, get_assignments_mock
 
         def it_should_query_for_the_given_section(system):
             _, get_assignments_mock = system
