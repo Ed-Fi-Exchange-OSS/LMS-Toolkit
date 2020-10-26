4d78a9a44e00e66ae044529077ba41ca91d31b8b
Stephen A. Fuqua <stephen@safnet.com>
2020-10-26 11:27:33 -0500

    Remove unused import

--- src/schoology-extractor/schoology_extractor/mapping/sections.py
+++ src/schoology-extractor/schoology_extractor/mapping/sections.py
@@ -3,8 +3,6 @@
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
 
-from datetime import datetime
-
 import pandas as pd
 
 from . import constants
