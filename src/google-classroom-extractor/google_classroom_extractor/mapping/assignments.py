7b791f86dd42fb6cc73f192bdbd200896db4257e
Brad Banister <bradbanister@users.noreply.github.com>
2020-11-05 10:16:40 -0600

    [FIZZ-160] include LMSSectionIdentifier in UDM assignment output (#74)

--- src/google-classroom-extractor/google_classroom_extractor/mapping/assignments.py
+++ src/google-classroom-extractor/google_classroom_extractor/mapping/assignments.py
@@ -37,6 +37,8 @@ def coursework_to_assignments_dfs(
         DueDateTime: The date and time the assignment is due
         EndDateTime: The end date and time for the assignment
         EntityStatus: The status of the record
+        LMSSectionSourceSystemIdentifier: A unique numeric identifier assigned
+            to the section
         MaxPoints: The maximum number of points a student may receive
         SourceSystem: The system code or name providing the assignment data
         SourceSystemIdentifier: A unique number or alphanumeric code assigned to
@@ -74,7 +76,9 @@ def coursework_to_assignments_dfs(
         if filled_df is None:
             raise ValueError  # fillna will never return None in this usage
         coursework_df["DueDateTime"] = filled_df.apply(
-            lambda date_element: datetime(*to_numeric(date_element, downcast="integer")),
+            lambda date_element: datetime(
+                *to_numeric(date_element, downcast="integer")
+            ),
             axis=1,
         )
     else:
@@ -101,7 +105,7 @@ def coursework_to_assignments_dfs(
 
     assignments_df = assignments_df.rename(
         columns={
-            "courseId": "SourceSystemSectionIdentifier",
+            "courseId": "LMSSectionSourceSystemIdentifier",
             "workType": "AssignmentCategory",
             "description": "AssignmentDescription",
             "scheduledTime": "StartDateTime",
@@ -118,11 +122,7 @@ def coursework_to_assignments_dfs(
 
     # group by section id as a Dict of DataFrames
     result: Dict[str, DataFrame] = dict(
-        tuple(assignments_df.groupby(["SourceSystemSectionIdentifier"]))
+        tuple(assignments_df.groupby(["LMSSectionSourceSystemIdentifier"]))
     )
-
-    # no longer need group by column
-    for grouped_df in result.values():
-        grouped_df.drop(columns=["SourceSystemSectionIdentifier"], inplace=True)
 
     return result
