caa0e1b3cc11f13a642517bf71f4f989ed6c6614
Stephen A. Fuqua <stephen@safnet.com>
2020-11-05 10:55:32 -0600

    Re-arrange main and add more logging

--- src/google-classroom-extractor/google_classroom_extractor/main.py
+++ src/google-classroom-extractor/google_classroom_extractor/main.py
@@ -8,6 +8,7 @@ from datetime import datetime
 import logging
 import os
 import sys
+from typing import Optional
 
 from dotenv import load_dotenv
 from googleapiclient.discovery import build, Resource
@@ -18,11 +19,17 @@ from google_classroom_extractor.result import Result
 from google_classroom_extractor.config import get_credentials, get_sync_db_engine
 from google_classroom_extractor.request import request_all
 from google_classroom_extractor.mapping.users import students_and_teachers_to_users_df
-from google_classroom_extractor.mapping.user_section_associations import students_and_teachers_to_user_section_associations_dfs
+from google_classroom_extractor.mapping.user_section_associations import (
+    students_and_teachers_to_user_section_associations_dfs,
+)
 from google_classroom_extractor.mapping.sections import courses_to_sections_df
 from google_classroom_extractor.mapping.assignments import coursework_to_assignments_dfs
-from google_classroom_extractor.mapping.assignment_submissions import submissions_to_assignment_submissions_dfs
-from google_classroom_extractor.mapping.user_submission_activities import submissions_to_user_submission_activities_dfs
+from google_classroom_extractor.mapping.assignment_submissions import (
+    submissions_to_assignment_submissions_dfs,
+)
+from google_classroom_extractor.mapping.user_submission_activities import (
+    submissions_to_user_submission_activities_dfs,
+)
 from google_classroom_extractor.csv_generation.write import (
     write_csv,
     write_multi_csv,
@@ -35,9 +42,12 @@ from google_classroom_extractor.csv_generation.write import (
     USER_ACTIVITY_ROOT_DIRECTORY,
 )
 
+logger: logging.Logger
 
-def request() -> Result:
-    load_dotenv()
+
+def configure_logging():
+    global logger
+
     logging.basicConfig(
         handlers=[
             logging.StreamHandler(sys.stdout),
@@ -47,24 +57,36 @@ def request() -> Result:
     )
     logging.getLogger("matplotlib").setLevel(logging.ERROR)
     logging.getLogger("opnieuw").setLevel(logging.DEBUG)
-
-    credentials: service_account.Credentials = get_credentials()
-    reports_resource: Resource = build(
-        "admin", "reports_v1", credentials=credentials, cache_discovery=False
-    )
-    classroom_resource: Resource = build(
-        "classroom", "v1", credentials=credentials, cache_discovery=False
-    )
-
-    sync_db: sqlalchemy.engine.base.Engine = get_sync_db_engine()
-
-    return request_all(classroom_resource, reports_resource, sync_db)
+    logger = logging.getLogger(__name__)
 
 
-if __name__ == "__main__":
+def request() -> Optional[Result]:
+    try:
+        credentials: service_account.Credentials = get_credentials()
+        reports_resource: Resource = build(
+            "admin", "reports_v1", credentials=credentials, cache_discovery=False
+        )
+        classroom_resource: Resource = build(
+            "classroom", "v1", credentials=credentials, cache_discovery=False
+        )
+
+        sync_db: sqlalchemy.engine.base.Engine = get_sync_db_engine()
+
+        return request_all(classroom_resource, reports_resource, sync_db)
+    except Exception:
+        logger.exception("An exception occurred while connecting to the API")
+        return None
+
+
+def main():
+    logger.info("Starting Ed-Fi LMS Google Classroom Extractor")
     result_dfs: Result = request()
 
-    logging.info("Writing LMS UDM Users to CSV file")
+    if not result_dfs:
+        # Enrich this in FIZZ-150
+        return
+
+    logger.info("Writing LMS UDM Users to CSV file")
     write_csv(
         students_and_teachers_to_users_df(
             result_dfs.students_df, result_dfs.teachers_df
@@ -73,37 +95,47 @@ if __name__ == "__main__":
         USERS_ROOT_DIRECTORY,
     )
 
-    logging.info("Writing LMS UDM Sections to CSV file")
+    logger.info("Writing LMS UDM Sections to CSV file")
     write_csv(
         courses_to_sections_df(result_dfs.courses_df),
         datetime.now(),
         SECTIONS_ROOT_DIRECTORY,
     )
 
-    logging.info("Writing LMS UDM UserSectionAssociations to CSV files")
+    logger.info("Writing LMS UDM UserSectionAssociations to CSV files")
     write_multi_csv(
-        students_and_teachers_to_user_section_associations_dfs(result_dfs.students_df, result_dfs.teachers_df),
+        students_and_teachers_to_user_section_associations_dfs(
+            result_dfs.students_df, result_dfs.teachers_df
+        ),
         datetime.now(),
         SECTION_ASSOCIATIONS_ROOT_DIRECTORY,
     )
 
-    logging.info("Writing LMS UDM Assignments to CSV files")
+    logger.info("Writing LMS UDM Assignments to CSV files")
     write_multi_csv(
         coursework_to_assignments_dfs(result_dfs.coursework_df),
         datetime.now(),
         ASSIGNMENT_ROOT_DIRECTORY,
     )
 
-    logging.info("Writing LMS UDM AssignmentSubmissions to CSV files")
+    logger.info("Writing LMS UDM AssignmentSubmissions to CSV files")
     write_multi_tuple_csv(
         submissions_to_assignment_submissions_dfs(result_dfs.submissions_df),
         datetime.now(),
         SUBMISSION_ROOT_DIRECTORY,
     )
 
-    logging.info("Writing LMS UDM User Activities to CSV files")
+    logger.info("Writing LMS UDM User Activities to CSV files")
     write_multi_csv(
         submissions_to_user_submission_activities_dfs(result_dfs.submissions_df),
         datetime.now(),
         USER_ACTIVITY_ROOT_DIRECTORY,
     )
+
+    logger.info("Finishing Ed-Fi LMS Google Classroom Extractor")
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    configure_logging()
+    main()
