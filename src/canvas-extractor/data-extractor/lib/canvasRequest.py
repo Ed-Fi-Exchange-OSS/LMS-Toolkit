74e6db6caf65bf7b8dfb9e5f276e6f964fe15ff8
Luis Richmond <lalrg@outlook.com>
2020-11-12 08:53:35 -0600

    Add basic error handling for requests

--- src/canvas-extractor/data-extractor/lib/canvasRequest.py
+++ src/canvas-extractor/data-extractor/lib/canvasRequest.py
@@ -6,10 +6,20 @@
 import requests
 import os
 
+
 def get(url):
     base_url = os.getenv("CANVAS_BASE_URL")
     auth_token = os.getenv("CANVAS_ACCESS_TOKEN")
     r = requests.get(
         base_url + "/" + url,
-        headers={ "Authorization" : f"Bearer {auth_token}" },)
-    return r.json()
+        headers={"Authorization" : f"Bearer {auth_token}"},)
+    json_response = r.json()
+    if "errors" in json_response:
+        print(f'The `{url}` endpoind responded with errors: {json_response["errors"]}')
+        raise CanvasRequestError('The execution of the script has been interrupted')
+
+    return json_response
+
+
+class CanvasRequestError(Exception):
+    pass
