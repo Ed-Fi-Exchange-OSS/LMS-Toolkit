5b3fed94c25259a793d75b4bccb9f24d9e842308
Stephen A. Fuqua <stephen@safnet.com>
2020-11-12 14:36:50 -0600

    further test refactoring

--- src/schoology-extractor/tests/helpers/test_arg_parser.py
+++ src/schoology-extractor/tests/helpers/test_arg_parser.py
@@ -3,6 +3,9 @@
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
 
+from logging import WARNING
+from typing import List
+
 import pytest
 
 from schoology_extractor.helpers.arg_parser import (
@@ -11,6 +14,7 @@ from schoology_extractor.helpers.arg_parser import (
     parse_grading_periods_arguments,
     GradingPeriodsArguments,
 )
+
 
 @pytest.fixture
 def required_params_for_parse_grading_periods_arguments():
@@ -25,21 +29,69 @@ def assert_error_message(capsys):
 
 
 def describe_when_parsing_arguments():
-    @pytest.fixture
-    def required_params_for_parse_main_arguments():
-        return ["-s", "fake_secret", "-g", "fake_grading_periods", "-k", "fake_key"]
+    def describe_given_minimum_parameters():
+        @pytest.fixture
+        def result() -> MainArguments:
+            # Arrange
+            parameters = ["-s", "fake_secret", "-g", "123,456", "-k", "fake_key"]
 
-    def describe_given_parameters_are_valid():
-        def it_should_not_throw_exception(required_params_for_parse_main_arguments):
-            parse_main_arguments(required_params_for_parse_main_arguments)
+            # Act
+            return parse_main_arguments(parameters)
 
-        def it_should_return_correct_type(required_params_for_parse_main_arguments):
-            parsed_arguments = parse_main_arguments(
-                required_params_for_parse_main_arguments
-            )
-            assert isinstance(parsed_arguments, MainArguments)
+        def it_should_load_the_secret(result: MainArguments):
+            assert result.client_secret == "fake_secret"
+
+        def it_should_load_the_grading_periods(result: MainArguments):
+            assert result.grading_periods == "123,456"
+
+        def it_should_load_the_key(result: MainArguments):
+            assert result.client_key == "fake_key"
+
+        def it_should_default_to_warn_level(result: MainArguments):
+            assert result.log_level == "WARNING"
+
+        def it_should_default_to_page_size_20(result: MainArguments):
+            assert result.page_size == 20
+
+        def it_should_default_to_current_directory(result: MainArguments):
+            assert result.output_directory == ""
+
+    def describe_given_optional_parameters():
+        @pytest.fixture
+        def result() -> MainArguments:
+            # Arrange
+            parameters = [
+                "-s",
+                "fake_secret",
+                "-g",
+                "123,456",
+                "-k",
+                "fake_key",
+                "-o",
+                "output_directory",
+                "-l",
+                "DEBUG",
+                "-p",
+                "30",
+            ]
+
+            # Act
+            return parse_main_arguments(parameters)
+
+        def it_should_load_the_output_directory(result: MainArguments):
+            assert result.output_directory == "output_directory"
+
+        def it_should_load_the_log_level(result: MainArguments):
+            assert result.log_level == "DEBUG"
+
+        def it_should_load_the_page_size(result: MainArguments):
+            assert result.page_size == 30
 
     def describe_given_parameters_are_not_valid():
+        @pytest.fixture
+        def default_parameters() -> List[str]:
+            return ["-s", "fake_secret", "-g", "123,456", "-k", "fake_key"]
+
         def given_client_key_parameter_is_missing_then_throw_error(capsys):
             args = ["-s", "fake_secret", "-g", "fake_grading_periods"]
             with pytest.raises(SystemExit):
@@ -58,24 +110,24 @@ def describe_when_parsing_arguments():
                 parse_main_arguments(args)
                 assert_error_message(capsys)
 
-        def given_not_valid_log_level_parameter_is_missing_then_throw_system_exit_error(
-            required_params_for_parse_main_arguments, capsys
+        def given_invalid_log_level_parameter_then_throw_system_exit_error(
+            default_parameters, capsys
         ):
             args = [
                 "-l",
                 "invalid_log_level",
-            ] + required_params_for_parse_main_arguments
+            ] + default_parameters
             with pytest.raises(SystemExit):
                 parse_main_arguments(args)
                 assert_error_message(capsys)
 
-        def given_not_valid_page_size_parameter_then_throw_system_exit_error(
-            required_params_for_parse_main_arguments, capsys
+        def given_invalid_page_size_parameter_then_throw_system_exit_error(
+            default_parameters, capsys
         ):
             args = [
                 "-p",
                 "non_numeric_value",
-            ] + required_params_for_parse_main_arguments
+            ] + default_parameters
             with pytest.raises(SystemExit):
                 parse_main_arguments(args)
                 assert_error_message(capsys)
