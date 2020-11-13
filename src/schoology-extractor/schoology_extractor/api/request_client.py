9998fc7e7b7619fd5d434a126f426be7fa9b2b19
Luis Richmond <lalrg@outlook.com>
2020-11-13 11:40:59 -0600

    Add get_all_pages method to request_client Refactor request_client and schoology_extract_facade

--- src/schoology-extractor/schoology_extractor/api/request_client.py
+++ src/schoology-extractor/schoology_extractor/api/request_client.py
@@ -7,7 +7,7 @@ from dataclasses import dataclass
 import os
 import time
 import random
-from typing import List, Union
+from typing import Union
 
 from opnieuw import retry
 from requests.exceptions import ConnectionError, HTTPError, Timeout
@@ -46,7 +46,6 @@ class RequestClient:
     oauth : OAuth1Session
         The two-legged authenticated OAuth1 session.
     """
-
     schoology_key: str
     schoology_secret: str
     base_url: str = DEFAULT_URL
@@ -56,14 +55,15 @@ class RequestClient:
 
     @property
     def _request_header(self) -> dict:
-        """The _request_header property builds the Request Header for oauth requests
+        """
+        The _request_header property builds the Request Header for oauth requests
 
         Returns
         -------
         dict
             Request headers.
-
         """
+
         assert isinstance(
             self.schoology_key, str
         ), "Property `schoology_key` should be of type `str`."
@@ -108,7 +108,7 @@ class RequestClient:
 
         Parameters
         ----------
-        resource : string
+        resource : str
             The resource endpoint that you want to request.
 
         Returns
@@ -116,6 +116,7 @@ class RequestClient:
         dict
             A parsed response from the server
         """
+
         assert isinstance(resource, str), "Argument `resource` should be of type `str`."
         assert isinstance(
             self.base_url, str
@@ -134,129 +135,57 @@ class RequestClient:
 
         return response.json()
 
-    def get_assignments_by_section_ids(
-        self, section_ids: Union[List[str], List[int]], page_size: int = DEFAULT_PAGE_SIZE
-    ) -> list:
-        """
-        Parameters
-        ----------
-        section_ids : list
-            A list of section ids.
-
-        Returns
-        -------
-        dict
-            A parsed response from the server
-
-        Note
-        --------
-        This function will do one or more requests that return paginated results
-        and it will handle paging for each request by itself in order to just return
-        a simple list of items, instead of a list with PaginatedResults
-
-        """
-        assert isinstance(
-            section_ids, list
-        ), "Argument `section_ids` should be of type `list`."
-
-        assignments: List[object] = []
-        for section_id in section_ids:
-            params = self._build_query_params_for_first_page(page_size)
-            url = f"sections/{section_id}/assignments?{params}"
-
-            assignments_per_section = PaginatedResult(
-                self,
-                page_size,
-                self.get(url),
-                RESOURCE_NAMES.ASSIGNMENT,
-                self.base_url + url,
-            )
-            while True:
-                current_page_assignments = assignments_per_section.current_page_items
-
-                for assignment in current_page_assignments:
-                    assignment["section_id"] = section_id
-                assignments = assignments + current_page_assignments
-
-                if assignments_per_section.get_next_page() is None:
-                    break
-
-        return assignments
-
     def get_assignments(
         self, section_id: int, page_size: int = DEFAULT_PAGE_SIZE
-    ) -> list:
+    ) -> PaginatedResult:
         """
         Parameters
         ----------
         section_id : int
             A Section Id
+        page_size : int
+            Number of items per page
 
         Returns
         -------
-        dict
+        PaginatedResult
             A parsed response from the server
         """
 
-        assignments: List[object] = []
+        url = f"sections/{section_id}/assignments?{self._build_query_params_for_first_page(page_size)}"
 
-        params = self._build_query_params_for_first_page(page_size)
-        url = f"sections/{section_id}/assignments?{params}"
-
-        assignments_per_section = PaginatedResult(
+        return PaginatedResult(
             self,
             page_size,
             self.get(url),
-            "assignment",
+            RESOURCE_NAMES.ASSIGNMENT,
             self.base_url + url,
         )
-        while True:
-            assignments = assignments + assignments_per_section.current_page_items
 
-            if assignments_per_section.get_next_page() is None:
-                break
-
-        return assignments
-
-    def get_section_by_course_ids(
-        self, course_ids: list, page_size: int = DEFAULT_PAGE_SIZE
-    ) -> list:
+    def get_section_by_course_id(
+        self, course_id: Union[int, str], page_size: int = DEFAULT_PAGE_SIZE
+    ) -> PaginatedResult:
         """
         Parameters
         ----------
-        section_id : str
+        course_id : Union[int, str]
             The id of the section.
+        page_size : int
+            Number of items per page
 
         Returns
         -------
-        List[object]
+        PaginatedResult
             A parsed response from the server
-
-        Note
-        --------
-        This function will do one or more requests that return paginated results
-        and it will handle paging for each request by itself in order to just return
-        a simple list of items, instead of a list with PaginatedResults
         """
-        assert isinstance(
-            course_ids, list
-        ), "Argument `course_ids` should be of type `list`."
 
-        sections: List[object] = []
-        for course_id in course_ids:
-            url = f"courses/{course_id}/sections"
-            sections_per_course = PaginatedResult(
-                self, page_size,
-                self.get(url),
-                RESOURCE_NAMES.SECTION,
-                self.base_url + url
-            )
-            while True:
-                sections = sections + sections_per_course.current_page_items
-                if sections_per_course.get_next_page() is None:
-                    break
-
-        return sections
+        url = f"courses/{course_id}/sections"
+        return PaginatedResult(
+            self, page_size,
+            self.get(url),
+            RESOURCE_NAMES.SECTION,
+            self.base_url + url
+        )
 
     def get_submissions_by_section_id_and_grade_item_id(
         self, section_id: int, grade_item_id: int, page_size: int = DEFAULT_PAGE_SIZE
@@ -277,17 +206,15 @@ class RequestClient:
         -------
         PaginatedResult
             A parsed response from the server
-
         """
 
         query_params = self._build_query_params_for_first_page(page_size)
         url = f"sections/{section_id}/submissions/{grade_item_id}?{query_params}"
 
-        response = self.get(url)
         return PaginatedResult(
             self,
             page_size,
-            response,
+            self.get(url),
             RESOURCE_NAMES.REVISION,
             self.base_url + url
         )
@@ -305,16 +232,11 @@ class RequestClient:
         -------
         PaginatedResult
             A parsed response from the server
-
         """
-        assert isinstance(
-            page_size, int
-        ), "Argument `page_size` should be of type `int`."
+
         url = f"users?{self._build_query_params_for_first_page(page_size)}"
 
-        return PaginatedResult(
-            self, page_size, self.get(url), "user", self.base_url + url
-        )
+        return PaginatedResult(self, page_size, self.get(url), "user", self.base_url + url)
 
     def get_grading_periods(
         self, page_size: int = DEFAULT_PAGE_SIZE
@@ -331,18 +253,11 @@ class RequestClient:
         -------
         PaginatedResult
             A parsed response from the server
-
         """
-        assert isinstance(
-            page_size, int
-        ), "Argument `section_id` should be of type `int`."
 
         url = f"gradingperiods?{self._build_query_params_for_first_page(page_size)}"
-        response = self.get(url)
 
-        return PaginatedResult(
-            self, page_size, response, "gradingperiods", self.base_url + url
-        )
+        return PaginatedResult(self, page_size, self.get(url), "gradingperiods", self.base_url + url)
 
     def get_courses(self, page_size: int = DEFAULT_PAGE_SIZE) -> PaginatedResult:
         """
@@ -359,13 +274,13 @@ class RequestClient:
             A parsed response from the server
 
         """
+
         url = f"courses?{self._build_query_params_for_first_page(page_size)}"
-        response = self.get(url)
 
         return PaginatedResult(
             self,
             page_size,
-            response,
+            self.get(url),
             RESOURCE_NAMES.COURSE,
             self.base_url + url)
 
@@ -384,19 +299,19 @@ class RequestClient:
             A parsed response from the server
 
         """
+
         url = f"roles?{self._build_query_params_for_first_page(page_size)}"
-        response = self.get(url)
 
         return PaginatedResult(
             self,
             page_size,
-            response,
+            self.get(url),
             RESOURCE_NAMES.ROLE,
             self.base_url + url)
 
     def get_enrollments(
         self, section_id: int, page_size: int = DEFAULT_PAGE_SIZE
-    ) -> list:
+    ) -> PaginatedResult:
         """
         Retrieves enrollment data for a section, with support for paging.
 
@@ -404,36 +319,29 @@ class RequestClient:
         ----------
         section_id : int
             A Section Id
+        page_size : int
+            Number of items per page.
 
         Returns
         -------
-        list
-            A list of all parsed results from the server, for all pages
+        PaginatedResult
+            A parsed response from the server
         """
-
-        enrollments: List[object] = []
 
         params = self._build_query_params_for_first_page(page_size)
         url = f"sections/{section_id}/enrollments?{params}"
 
-        enrollments_page = PaginatedResult(
+        return PaginatedResult(
             self,
             page_size,
             self.get(url),
-            "enrollment",
+            RESOURCE_NAMES.ENROLLMENT,
             self.base_url + url,
         )
-        while True:
-            enrollments = enrollments + enrollments_page.current_page_items
-
-            if enrollments_page.get_next_page() is None:
-                break
-
-        return enrollments
 
     def get_attendance(
         self, section_id: int
-    ) -> list:
+    ) -> list[dict]:
         """
         Retrieves attendance event data for a section. Note: attendance does
         not support paging.
@@ -445,8 +353,13 @@ class RequestClient:
 
         Returns
         -------
-        list
+        list[dict]
             A list of all parsed results from the server
+
+        Notes
+        -----
+        The endpoind for this resource doesn't support pagination,
+        that's why it returns a list.
         """
 
         url = f"sections/{section_id}/attendance"
@@ -457,7 +370,7 @@ class RequestClient:
 
     def get_discussions(
         self, section_id: int
-    ) -> list:
+    ) -> list[dict]:
         """
         Retrieves discussions list for a section.
 
@@ -468,21 +381,23 @@ class RequestClient:
 
         Returns
         -------
-        list
+        list[dict]
             A list of all parsed results from the server
+
+        Notes
+        -----
+        The endpoind for this resource doesn't support pagination,
+        that's why it returns a list.
         """
 
-        url = f"sections/{section_id}/discussions"
-
-        result = self.get(url)
-
+        result = self.get(f"sections/{section_id}/discussions")
         return result["discussion"]
 
     def get_discussion_replies(
         self, section_id: int, discussion_id: Union[int, str]
-    ) -> list:
+    ) -> list[dict]:
         """
-        Retrieves discussion list for a section.
+        Retrieves replies list for a discussion.
 
         Parameters
         ----------
@@ -490,14 +405,41 @@ class RequestClient:
             A Section Id
         discussion_id : Union[int, str]
             The Id of the discussion
+
         Returns
         -------
-        list
+        list[dict]
             A list of all parsed results from the server
+
+        Notes
+        -----
+        The endpoind for this resource doesn't support pagination,
+        that's why it returns a list.
         """
 
-        url = f"sections/{section_id}/discussions/{discussion_id}/comments"
-
-        result = self.get(url)
-        # This endpoint does not support pagination
+        result = self.get(f"sections/{section_id}/discussions/{discussion_id}/comments")
         return result["comment"]
+
+
+def get_all_pages(result: PaginatedResult) -> list[dict]:
+    """
+    Returns all items from a PaginatedResult within all available pages
+
+    Parameters
+    ----------
+    result : PaginatedResult
+        A PaginatedResult object.
+
+    Returns
+    -------
+    list[dict]
+        A list of all parsed results
+    """
+
+    items = list[dict]()
+    while True:
+        items = items + result.current_page_items
+        if result.get_next_page() is None:
+            break
+
+    return items
