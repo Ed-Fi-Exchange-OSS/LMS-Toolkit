ce38869bef2118988398c45f8534d1e61e36893a
Stephen A. Fuqua <stephen@safnet.com>
2020-11-20 09:37:12 -0600

    Fix some typing and import problems

--- src/file-utils/lms_file_utils/file_reader.py
+++ src/file-utils/lms_file_utils/file_reader.py
@@ -3,12 +3,25 @@
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
 
-from typing import Callable, Optional
+from typing import Callable
 
 import pandas as pd
 
-import file_repository as fr
 
+from .file_repository import (
+    get_users_file,
+    get_sections_file,
+    get_section_associations_file,
+    get_user_activities_file,
+    get_assignments_file,
+    get_grades_file,
+    get_attendance_events_file,
+    get_submissions_file
+)
+
+
+def _default() -> pd.DataFrame:
+    return pd.DataFrame()
 
 def _read_csv(file: str) -> pd.DataFrame:
     if file:
@@ -16,46 +29,46 @@ def _read_csv(file: str) -> pd.DataFrame:
             file, engine="c", parse_dates=True, infer_datetime_format=True
         )
 
-    return pd.DataFrame()
+    return _default()
 
 
 def get_all_users(base_directory: str) -> pd.DataFrame:
-    file = fr.get_users_file(base_directory)
+    file = get_users_file(base_directory)
 
     if file is not None:
         return _read_csv(file)
 
-    return pd.DataFrame()
+    return _default()
 
 
 def get_all_sections(base_directory: str) -> pd.DataFrame:
-    file = fr.get_sections_file(base_directory)
+    file = get_sections_file(base_directory)
 
     if file is not None:
         return _read_csv(file)
 
-    return pd.DataFrame()
+    return _default()
 
 
 def get_section_associations(base_directory: str, section_id: int) -> pd.DataFrame:
-    file = fr.get_section_associations_file(base_directory, section_id)
+    file = get_section_associations_file(base_directory, section_id)
 
     if file is not None:
         return _read_csv(file)
 
-    return pd.DataFrame()
+    return _default()
 
 
 def _get_data_for_section(
     base_directory: str, sections: pd.DataFrame, callback: Callable
-) -> Optional[pd.DataFrame]:
+) -> pd.DataFrame:
     df = pd.DataFrame()
 
     if sections.empty:
         print(
             "No sections have been loaded, therefore no section sub-files can be read."
         )
-        return
+        return _default()
 
     for _, section_id in sections[["SourceSystemIdentifier"]].itertuples():
         sa = callback(base_directory, section_id)
@@ -73,12 +86,12 @@ def get_all_section_associations(
 
 
 def get_user_activities(base_directory: str, section_id: int) -> pd.DataFrame:
-    file = fr.get_user_activities_file(base_directory, section_id)
+    file = get_user_activities_file(base_directory, section_id)
 
     if file is not None:
         return _read_csv(file)
 
-    return pd.DataFrame()
+    return _default()
 
 
 def get_all_user_activities(
@@ -88,12 +101,12 @@ def get_all_user_activities(
 
 
 def get_assignments(base_directory: str, section_id: int) -> pd.DataFrame:
-    file = fr.get_assignments_file(base_directory, section_id)
+    file = get_assignments_file(base_directory, section_id)
 
     if file is not None:
         return _read_csv(file)
 
-    return pd.DataFrame()
+    return _default()
 
 
 def get_all_assignments(base_directory: str, sections: pd.DataFrame) -> pd.DataFrame:
@@ -103,12 +116,12 @@ def get_all_assignments(base_directory: str, sections: pd.DataFrame) -> pd.DataF
 def get_submissions(
     base_directory: str, section_id: int, assignment_id: int
 ) -> pd.DataFrame:
-    file = fr.get_submissions_file(base_directory, section_id, assignment_id)
+    file = get_submissions_file(base_directory, section_id, assignment_id)
 
     if file is not None:
         return _read_csv(file)
 
-    return pd.DataFrame()
+    return _default()
 
 
 def get_all_submissions(base_directory: str, assignments: pd.DataFrame) -> pd.DataFrame:
@@ -124,12 +137,12 @@ def get_all_submissions(base_directory: str, assignments: pd.DataFrame) -> pd.Da
 
 
 def get_grades(base_directory: str, section_id: int) -> pd.DataFrame:
-    file = fr.get_grades_file(base_directory, section_id)
+    file = get_grades_file(base_directory, section_id)
 
     if file is not None:
         return _read_csv(file)
 
-    return pd.DataFrame()
+    return _default()
 
 
 def get_all_grades(base_directory: str, sections: pd.DataFrame) -> pd.DataFrame:
@@ -137,12 +150,12 @@ def get_all_grades(base_directory: str, sections: pd.DataFrame) -> pd.DataFrame:
 
 
 def get_attendance_events(base_directory: str, section_id: int) -> pd.DataFrame:
-    file = fr.get_attendance_events_file(base_directory, section_id)
+    file = get_attendance_events_file(base_directory, section_id)
 
     if file is not None:
         return _read_csv(file)
 
-    return pd.DataFrame()
+    return _default()
 
 
 def get_all_attendance_events(
