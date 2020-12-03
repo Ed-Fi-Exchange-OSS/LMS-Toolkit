478bd3779d3d130cea1c5e98503d28d134074c85
Stephen A. Fuqua <stephen@safnet.com>
2020-12-03 10:21:41 -0600

    Try bypassing pyfakefs setUp

--- src/file-utils/tests/test_file_repository.py
+++ src/file-utils/tests/test_file_repository.py
@@ -5,6 +5,7 @@
 
 
 import pytest
+from pyfakefs.fake_filesystem import FakeFilesystem
 
 from lms_file_utils.file_repository import (
     get_users_file,
@@ -19,7 +20,7 @@ from lms_file_utils.file_repository import (
 )
 
 
-BASE_DIRECTORY = "./base_dir"
+BASE_DIRECTORY = "base_dir"
 SECTION_ID = 1
 ASSIGNMENT_ID = 2
 
@@ -80,181 +81,185 @@ files = [
 
 def describe_given_filesystem_does_not_exist():
     @pytest.fixture
-    def init_fs(fs):
-        # Fake as Linux so that all slashes in these test are forward
-        # fs.path_separator = "/"
-        # fs.is_windows_fs = False
-        # fs.is_macos = False
-        raise RuntimeError("try to fail here")
+    def init_fs():
+
+        # Manually creating this to avoid a strange error: when TeamCity calls
+        # the pyfakefs setUp function, it tries to create a base directory entry
+        # for C: and encounters a failure. Unable to reproduce manually.
+        fs = FakeFilesystem(path_separator='/')
         fs.os = "linux"
 
     def describe_when_getting_sections_file_file():
-        def it_should_return_None(fs, init_fs):
+        def it_should_return_None(init_fs):
             file = get_sections_file(BASE_DIRECTORY)
             assert file is None
 
     def describe_when_getting_users_file_file():
-        def it_should_return_None(fs, init_fs):
+        def it_should_return_None(init_fs):
             file = get_users_file(BASE_DIRECTORY)
             assert file is None
 
     def describe_when_getting_assignments_file():
-        def it_should_return_None(fs, init_fs):
+        def it_should_return_None(init_fs):
             file = get_assignments_file(BASE_DIRECTORY, SECTION_ID)
             assert file is None
 
     def describe_when_getting_section_associations_file():
-        def it_should_return_None(fs, init_fs):
+        def it_should_return_None(init_fs):
             file = get_section_associations_file(BASE_DIRECTORY, SECTION_ID)
             assert file is None
 
     def describe_when_getting_grades_file():
-        def it_should_return_None(fs, init_fs):
+        def it_should_return_None(init_fs):
             file = get_grades_file(BASE_DIRECTORY, SECTION_ID)
             assert file is None
 
     def describe_when_getting_submissions_file():
-        def it_should_return_None(fs, init_fs):
+        def it_should_return_None(init_fs):
             file = get_submissions_file(BASE_DIRECTORY, SECTION_ID, ASSIGNMENT_ID)
             assert file is None
 
     def describe_when_getting_attendance_events_file():
-        def it_should_return_None(fs, init_fs):
+        def it_should_return_None(init_fs):
             file = get_attendance_events_file(BASE_DIRECTORY, SECTION_ID)
             assert file is None
 
     def describe_when_getting_section_activities_file():
-        def it_should_return_None(fs, init_fs):
+        def it_should_return_None(init_fs):
             file = get_section_activities_file(BASE_DIRECTORY, SECTION_ID)
             assert file is None
 
     def describe_when_getting_system_activities_files():
-        def it_should_return_an_empty_list(fs, init_fs):
+        def it_should_return_an_empty_list(init_fs):
             files = get_system_activities_files(BASE_DIRECTORY)
             assert len(files) == 0
 
 
 def describe_given_filesystem_exists_with_no_files():
     @pytest.fixture
-    def init_fs(fs):
-        # Fake as Linux so that all slashes in these test are forward
-        fs.path_separator = "/"
-        fs.is_windows_fs = False
-        fs.is_macos = False
+    def init_fs():
 
-        fs.create_dir(f"{BASE_DIRECTORY}/sections")
-        fs.create_dir(f"{BASE_DIRECTORY}/sections/section={SECTION_ID}/assignments")
-        fs.create_dir(f"{BASE_DIRECTORY}/sections/section={SECTION_ID}/assignments/ASSIGNMENT={ASSIGNMENT_ID}/submissions")
-        fs.create_dir(f"{BASE_DIRECTORY}/sections/section={SECTION_ID}/attendance-events")
-        fs.create_dir(f"{BASE_DIRECTORY}/sections/section={SECTION_ID}/grades")
-        fs.create_dir(f"{BASE_DIRECTORY}/sections/section={SECTION_ID}/section-associations")
-        fs.create_dir(f"{BASE_DIRECTORY}/sections/section={SECTION_ID}/section-activities")
+        # Manually creating this to avoid a strange error: when TeamCity calls
+        # the pyfakefs setUp function, it tries to create a base directory entry
+        # for C: and encounters a failure. Unable to reproduce manually.
+        fs = FakeFilesystem(path_separator='/')
+        fs.os = "linux"
+
+        fs.create_dir(f"{BASE_DIRECTORY}/section={SECTION_ID}/assignments")
+        f = f"{BASE_DIRECTORY}/section={SECTION_ID}/ASSIGNMENT={ASSIGNMENT_ID}/submissions"
+        fs.create_dir(f)
+        fs.create_dir(f"{BASE_DIRECTORY}/section={SECTION_ID}/attendance-events")
+        fs.create_dir(f"{BASE_DIRECTORY}/section={SECTION_ID}/grades")
+        fs.create_dir(f"{BASE_DIRECTORY}/section={SECTION_ID}/section-associations")
+        fs.create_dir(f"{BASE_DIRECTORY}/section={SECTION_ID}/section-activities")
         fs.create_dir(f"{BASE_DIRECTORY}/system-activities/date=2020-11-17")
         fs.create_dir(f"{BASE_DIRECTORY}/system-activities/date=2020-11-18")
         fs.create_dir(f"{BASE_DIRECTORY}/users")
 
     def describe_when_getting_sections_file_file():
-        def it_should_return_None(fs, init_fs):
+        def it_should_return_None(init_fs):
             file = get_sections_file(BASE_DIRECTORY)
             assert file is None
 
     def describe_when_getting_users_file_file():
-        def it_should_return_None(fs, init_fs):
+        def it_should_return_None(init_fs):
             file = get_users_file(BASE_DIRECTORY)
             assert file is None
 
     def describe_when_getting_assignments_file():
-        def it_should_return_None(fs, init_fs):
+        def it_should_return_None(init_fs):
             file = get_assignments_file(BASE_DIRECTORY, SECTION_ID)
             assert file is None
 
     def describe_when_getting_section_associations_file():
-        def it_should_return_None(fs, init_fs):
+        def it_should_return_None(init_fs):
             file = get_section_associations_file(BASE_DIRECTORY, SECTION_ID)
             assert file is None
 
     def describe_when_getting_grades_file():
-        def it_should_return_None(fs, init_fs):
+        def it_should_return_None(init_fs):
             file = get_grades_file(BASE_DIRECTORY, SECTION_ID)
             assert file is None
 
     def describe_when_getting_submissions_file():
-        def it_should_return_None(fs, init_fs):
+        def it_should_return_None(init_fs):
             file = get_submissions_file(BASE_DIRECTORY, SECTION_ID, ASSIGNMENT_ID)
             assert file is None
 
     def describe_when_getting_attendance_events_file():
-        def it_should_return_None(fs, init_fs):
+        def it_should_return_None(init_fs):
             file = get_attendance_events_file(BASE_DIRECTORY, SECTION_ID)
             assert file is None
 
     def describe_when_getting_section_activities_file():
-        def it_should_return_None(fs, init_fs):
+        def it_should_return_None(init_fs):
             file = get_section_activities_file(BASE_DIRECTORY, SECTION_ID)
             assert file is None
 
     def describe_when_getting_system_activities_file():
-        def it_should_return_an_empty_list(fs, init_fs):
+        def it_should_return_an_empty_list(init_fs):
             file = get_system_activities_files(BASE_DIRECTORY)
             assert len(file) == 0
 
 
 def describe_given_files_exist():
     @pytest.fixture
-    def init_fs(fs):
-        # Fake as Linux so that all slashes in these test are forward
-        fs.path_separator = "/"
-        fs.is_windows_fs = False
-        fs.is_macos = False
+    def init_fs():
+
+        # Manually creating this to avoid a strange error: when TeamCity calls
+        # the pyfakefs setUp function, it tries to create a base directory entry
+        # for C: and encounters a failure. Unable to reproduce manually.
+        fs = FakeFilesystem(path_separator='/')
+        fs.os = "linux"
 
         for f in files:
             fs.create_file(f)
 
     def describe_when_getting_sections_file_file():
-        def it_should_return_newest_file(fs, init_fs):
+        def it_should_return_newest_file(init_fs):
             file = get_sections_file(BASE_DIRECTORY)
             assert file == SECTION_FILE_NEW
 
     def describe_when_getting_users_file_file():
-        def it_should_return_newest_file(fs, init_fs):
+        def it_should_return_newest_file(init_fs):
             file = get_users_file(BASE_DIRECTORY)
             assert file == USERS_FILE_NEW
 
     def describe_when_getting_assignments_file():
-        def it_should_return_newest_file(fs, init_fs):
+        def it_should_return_newest_file(init_fs):
             file = get_assignments_file(BASE_DIRECTORY, SECTION_ID)
             assert file == ASSIGNMENTS_FILE_NEW
 
     def describe_when_getting_section_associations_file():
-        def it_should_return_newest_file(fs, init_fs):
+        def it_should_return_newest_file(init_fs):
             file = get_section_associations_file(BASE_DIRECTORY, SECTION_ID)
             assert file == ASSOCIATIONS_FILE_NEW
 
     def describe_when_getting_grades_file():
-        def it_should_return_newest_file(fs, init_fs):
+        def it_should_return_newest_file(init_fs):
             file = get_grades_file(BASE_DIRECTORY, SECTION_ID)
             assert file == GRADES_FILE_NEW
 
     def describe_when_getting_submissions_file():
-        def it_should_return_newest_file(fs, init_fs):
+        def it_should_return_newest_file(init_fs):
             file = get_submissions_file(BASE_DIRECTORY, SECTION_ID, ASSIGNMENT_ID)
             assert file == SUBMISSIONS_FILE_NEW
 
     def describe_when_getting_attendance_events_file():
-        def it_should_return_newest_file(fs, init_fs):
+        def it_should_return_newest_file(init_fs):
             file = get_attendance_events_file(BASE_DIRECTORY, SECTION_ID)
             assert file == ATTENDANCE_FILE_NEW
 
     def describe_when_getting_section_activities_file():
-        def it_should_return_newest_file(fs, init_fs):
+        def it_should_return_newest_file(init_fs):
             file = get_section_activities_file(BASE_DIRECTORY, SECTION_ID)
             assert file == SECTION_ACTIVITIES_FILE_NEW
 
     def describe_when_getting_system_activities_files():
-        def it_return_newest_file_from_day_one(fs, init_fs):
+        def it_return_newest_file_from_day_one(init_fs):
             files = get_system_activities_files(BASE_DIRECTORY)
             assert SYSTEM_ACTIVITIES_FILE_NEW_1 in files
 
-        def it_return_newest_file_from_day_two(fs, init_fs):
+        def it_return_newest_file_from_day_two(init_fs):
             files = get_system_activities_files(BASE_DIRECTORY)
             assert SYSTEM_ACTIVITIES_FILE_NEW_2 in files
