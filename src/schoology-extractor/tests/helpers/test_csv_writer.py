55c0f66e1852eb51f2b60c416e914953dc7aeaa8
Stephen A. Fuqua <stephen@safnet.com>
2020-12-10 08:22:04 -0600

    Remove old functions Write empty files

--- src/schoology-extractor/tests/helpers/test_csv_writer.py
+++ src/schoology-extractor/tests/helpers/test_csv_writer.py
@@ -8,12 +8,12 @@ import os
 import pytest
 import pandas as pd
 
-from schoology_extractor.helpers.csv_writer import df_to_csv, to_csv, to_string
+from schoology_extractor.helpers.csv_writer import df_to_csv
 
 
-def describe_when_exporting_data():
-    def describe_when_writing_dataframe_to_csv():
-        def it_should_not_include_index(fs):
+def describe_when_writing_dataframe_to_csv():
+    def describe_given_DataFrame_has_data():
+        def the_output_should_not_include_the_index(fs):
             path = "a/b.csv"
             df = pd.DataFrame([{"a": 1}, {"a": 2}])
             expected = "a\n1\n2\n"
@@ -29,50 +29,25 @@ def describe_when_exporting_data():
                 contents = f.read()
                 assert expected == contents
 
-        def it_should_not_write_file_for_empty_DataFrame(fs):
+    def describe_given_DataFrame_is_empty():
+        def it_should_write_file_for_empty_DataFrame(fs):
+            # Arrange
+            path = "path"
+
             # Act
-            df_to_csv(pd.DataFrame(), "path")
+            df_to_csv(pd.DataFrame(), path)
 
             # Assert
-            assert not os.path.exists("path")
+            assert os.path.exists(path)
 
-    def describe_when_writing_list_to_csv():
-        def given_no_data_parameter_then_throw_assert_exception():
-            with pytest.raises(AssertionError):
-                to_csv(None, "")  # type: ignore
-
-        def test_given_no_output_parameter_then_throw_assert_exception():
-            with pytest.raises(AssertionError):
-                to_csv([], None)  # type: ignore
-
-        def it_should_write_to_file(fs):
-            path = "a/b.csv"
-            input = [{"a": 1}, {"a": 2}]
-            expected = "a\n1\n2\n"
-
+        def and_the_file_should_be_empty(fs):
             # Arrange
-            fs.create_dir("a")
+            path = "path"
 
             # Act
-            to_csv(input, path)
+            df_to_csv(pd.DataFrame(), path)
 
             # Assert
             with open(path) as f:
                 contents = f.read()
-                assert expected == contents
-
-    def describe_when_to_string_method_is_called():
-        def given_no_data_parameter_then_throw_assert_exception():
-            with pytest.raises(AssertionError):
-                to_string(None)  # type: ignore
-
-        def it_should_call_DataFrame_method():
-            # Arrange
-            fake_data = [{"test": "test"}, {"test": "test"}]
-            expected_result = "   test\n0  test\n1  test"
-
-            # Act
-            result = to_string(fake_data)
-
-            # Assert
-            assert result == expected_result
+                assert "\n" == contents
