362dc08b7f786dc77629439c2183ce3251a12c5b
Luis Richmond <lalrg@outlook.com>
2021-01-06 12:42:07 -0600

    [FIZZ-135] Map assignment-submissions (#127)
    * Map submissions
    
    * Add unit tests
    
    * Fix types
    
    * lowerCase non-constant values
    
    * Load output directory from environment variable
    
    * Extract logic from __main__ into facade
    
    * Fix code
    
    * Change imports and add unit tests
    
    * Fix typing issue
    
    * Ignore test.db

---
+++ src/canvas-extractor/canvas_extractor/extract_facade.py
@@ -0,0 +1,214 @@
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
+from typing import Dict, List, Tuple
+
+from canvasapi import Canvas
+from canvasapi.enrollment import Enrollment
+import sqlalchemy
+from canvasapi.course import Course
+from canvasapi.section import Section
+from canvasapi.user import User
+from canvasapi.assignment import Assignment
+from canvasapi.submission import Submission
+from pandas import DataFrame
+
+from canvas_extractor.api import (
+    courses as coursesApi,
+    sections as sectionsApi,
+    students as studentsApi,
+    assignments as assignmentsApi,
+    submissions as submissionsApi,
+    enrollments as enrollmentsApi,
+)
+from canvas_extractor.mapping import (
+    sections as sectionsMap,
+    users as usersMap,
+    assignments as assignmentsMap,
+    submissions as submissionsMap,
+    section_associations as section_associationsMap,
+)
+
+
+def extract_courses(
+    canvas: Canvas, sync_db: sqlalchemy.engine.base.Engine
+) -> Tuple[List[Course], DataFrame]:
+    """
+    Gets all Canvas courses, in the Ed-Fi UDM format.
+
+    Parameters
+    ----------
+    canvas: Canvas
+        Canvas object.
+    sync_db: sqlalchemy.engine.base.Engine
+        sync db.
+
+    Returns
+    -------
+    Tuple[List[Course], DataFrame]
+        A tuple with the list of Canvas Course objects and the udm_courses dataframe.
+    """
+    courses: List[Course] = coursesApi.request_courses(canvas)
+    courses_df: DataFrame = coursesApi.courses_synced_as_df(courses, sync_db)
+
+    return (courses, courses_df)
+
+
+def extract_sections(
+    courses: List[Course], sync_db: sqlalchemy.engine.base.Engine
+) -> Tuple[List[Section], DataFrame]:
+    """
+    Gets all Canvas sections, in the Ed-Fi UDM format.
+
+    Parameters
+    ----------
+    courses: List[Course]
+        A list of Canvas Course objects.
+    sync_db: sqlalchemy.engine.base.Engine
+        sync db.
+
+    Returns
+    -------
+    Tuple[List[Section], DataFrame]
+        A tuple with the list of Canvas Section objects and the udm_sections dataframe.
+    """
+    sections: List[Section] = sectionsApi.request_sections(courses)
+    sections_df: DataFrame = sectionsApi.sections_synced_as_df(sections, sync_db)
+    udm_sections_df: DataFrame = sectionsMap.map_to_udm_sections(sections_df)
+
+    return (sections, udm_sections_df)
+
+
+def extract_students(
+    courses: List[Course], sync_db: sqlalchemy.engine.base.Engine
+) -> Tuple[List[User], DataFrame]:
+    """
+    Gets all Canvas students, in the Ed-Fi UDM format.
+
+    Parameters
+    ----------
+    courses: List[Course]
+        A list of Canvas Course objects.
+    sync_db: sqlalchemy.engine.base.Engine
+        sync db.
+
+    Returns
+    -------
+    Tuple[List[User], DataFrame]
+        A tuple with the list of Canvas User objects and the udm_users dataframe.
+    """
+    students: List[User] = studentsApi.request_students(courses)
+    students_df: DataFrame = studentsApi.students_synced_as_df(students, sync_db)
+    udm_students_df: DataFrame = usersMap.map_to_udm_users(students_df)
+
+    return (students, udm_students_df)
+
+
+def extract_assignments(
+    courses: List[Course],
+    sections_df: DataFrame,
+    sync_db: sqlalchemy.engine.base.Engine,
+) -> Tuple[List[Assignment], DataFrame]:
+    """
+    Gets all Canvas assignments, in the Ed-Fi UDM format.
+
+    Parameters
+    ----------
+    courses: List[Course]
+        A list of Canvas Course objects.
+    sections_df: DataFrame
+        A DataFrame of Canvas Section objects.
+    sync_db: sqlalchemy.engine.base.Engine
+        sync db.
+
+    Returns
+    -------
+    Tuple[List[Assignment], DataFrame]
+        A tuple with the list of Canvas Assignment objects and the udm_assignments dataframe.
+    """
+    assignments: List[Assignment] = assignmentsApi.request_assignments(courses)
+    assignments_df: DataFrame = assignmentsApi.assignments_synced_as_df(
+        assignments, sync_db
+    )
+    udm_assignments_dfs: Dict[str, DataFrame] = assignmentsMap.map_to_udm_assignments(
+        assignments_df, sections_df
+    )
+
+    return (assignments, udm_assignments_dfs)
+
+
+def extract_submissions(
+    assignments: List[Assignment],
+    sections: List[Section],
+    sync_db: sqlalchemy.engine.base.Engine,
+) -> Dict[Tuple[str, str], DataFrame]:
+    """
+    Gets all Canvas submissions, in the Ed-Fi UDM format.
+
+    Parameters
+    ----------
+    assignments: List[Assignment]
+        A list of Canvas Assignment objects.
+    sections: List[Section]
+        A List of Canvas Section objects.
+    sync_db: sqlalchemy.engine.base.Engine
+        sync db.
+
+    Returns
+    -------
+    Dict[Tuple[str, str], DataFrame]
+        A dict with (section_id, assignment_id) as key and udm_submissions
+        as value.
+    """
+    export: Dict[Tuple[str, str], DataFrame] = {}
+    for section in sections:
+        for assignment in [
+            assignment
+            for assignment in assignments
+            if assignment.course_id == section.course_id
+        ]:
+            submissions: List[Submission] = submissionsApi.request_submissions(
+                assignment
+            )
+            submissions_df: DataFrame = submissionsApi.submissions_synced_as_df(
+                submissions, sync_db
+            )
+            submissions_df = submissionsMap.map_to_udm_submissions(submissions_df)
+            export[(section.id, assignment.id)] = submissions_df
+    return export
+
+
+def extract_enrollments(
+    sections: List[Section], sync_db: sqlalchemy.engine.base.Engine
+) -> Dict[str, DataFrame]:
+    """
+    Gets all Canvas enrollments, in the Ed-Fi UDM format.
+
+    Parameters
+    ----------
+    sections: List[Section]
+        A list of Canvas Section objects.
+    sync_db: sqlalchemy.engine.base.Engine
+        sync db.
+
+    Returns
+    -------
+    Dict[str, DataFrame]
+        A dict with section_id as key and udm_enrollments as value.
+    """
+    output: Dict[str, DataFrame] = dict()
+    for section in sections:
+        enrollments: List[Enrollment] = enrollmentsApi.request_enrollments_for_section(
+            section
+        )
+        enrollments_df: DataFrame = enrollmentsApi.enrollments_synced_as_df(
+            enrollments, sync_db
+        )
+        enrollments_df = section_associationsMap.map_to_udm_section_associations(
+            enrollments_df
+        )
+        output[section.id] = enrollments_df
+
+    return output
---
+++ src/canvas-extractor/canvas_extractor/mapping/submissions.py
@@ -0,0 +1,103 @@
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
+from datetime import datetime
+
+import pandas as pd
+
+from . import constants
+
+
+def _get_date_formated(date) -> str:
+    if not isinstance(date, str) or date == "":
+        return ""
+    return datetime.strftime(
+        datetime.strptime(date, "%Y-%m-%dT%H:%M:%S%z"), "%Y/%m/%d %H:%M:%S"
+    )
+
+
+def _get_status(row: pd.Series):
+    if row["late"] == 1:
+        return "late"
+    if row["missing"] == 1:
+        return "missing"
+    if row["graded_at"] is not None:
+        return "graded"
+    return "on-time"
+
+
+def map_to_udm_submissions(submissions_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Maps a DataFrame containing Canvas submissions into the Ed-Fi LMS Unified Data
+    Model (UDM) format.
+
+    Parameters
+    ----------
+    submissions_df: DataFrame
+        Pandas DataFrame containing Canvas submissions for a section
+
+    Returns
+    -------
+    DataFrame
+        A Submissions-formatted DataFrame .
+
+    Notes
+    -----
+    DataFrame columns are:
+        SourceSystemIdentifier: A unique number or alphanumeric code assigned to a
+            submission by the source system
+        SourceSystem: The system code or name providing the user data
+        SubmissionStatus: The status of the submission
+        SubmissionDateTime: Datetime of the submission
+        EarnedPoints: Earned points for the submission
+        Grade: Grade for the submission
+        AssignmentSourceSystemIdentifier: Unique identifier for the assignment
+        LMSUserSourceSystemIdentifier: Unique identifier of the LMSUser
+        EntityStatus: Status of the entity
+        CreateDate: Created date
+        LastModifiedDate: Last modified date
+        SourceCreateDate: Date this record was created in the LMS
+        SourceLastModifiedDate: Date this record was last updated in the LMS
+    """
+
+    if submissions_df.empty:
+        return submissions_df
+
+    df = submissions_df[
+        [
+            "id",
+            "late",
+            "missing",
+            "submitted_at",
+            "grade",
+            "assignment_id",
+            "user_id",
+            "CreateDate",
+            "LastModifiedDate",
+            "graded_at"
+        ]
+    ].copy()
+
+    df["submitted_at"] = df["submitted_at"].apply(_get_date_formated)
+
+    df.rename(
+        columns={
+            "id": "SourceSystemIdentifier",
+            "submitted_at": "SubmissionDateTime",
+            "assignment_id": "AssignmentSourceSystemIdentifier",
+            "user_id": "LMSUserSourceSystemIdentifier",
+            "grade": "Grade"
+        }, inplace=True)
+
+    df["SourceSystem"] = constants.SOURCE_SYSTEM
+    df["SubmissionStatus"] = df.apply(_get_status, axis=1)
+    df["EntityStatus"] = constants.ACTIVE
+    df["EarnedPoints"] = None
+    df["SourceCreateDate"] = None
+    df["SourceLastModifiedDate"] = None
+
+    df.drop(columns=["late", "missing", "graded_at"], inplace=True)
+
+    return df
