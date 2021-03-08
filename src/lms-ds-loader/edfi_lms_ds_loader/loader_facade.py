5a75c0842037511101a4bd023f276ec5845c6cd8
Stephen A. Fuqua <stephen@safnet.com>
2021-03-08 10:49:32 -0600

    Cleanup in the facade

--- src/lms-ds-loader/edfi_lms_ds_loader/loader_facade.py
+++ src/lms-ds-loader/edfi_lms_ds_loader/loader_facade.py
@@ -3,9 +3,12 @@
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
 
-import logging
+# Developer note: this module is deliberately not unit tested. As a facade it is
+# by definition complex. In this application, it contains the main application
+# logic and is tested simply by running the application. Any branching or
+# looping logic should go into other modules where they can be tested easily.
 
-from sqlalchemy import create_engine
+import logging
 
 from edfi_lms_ds_loader.helpers.constants import Table
 from edfi_lms_ds_loader.helpers.argparser import MainArguments
@@ -18,15 +21,20 @@ logger = logging.getLogger(__name__)
 
 
 def _load_users(csv_path: str, db_adapter: MssqlLmsOperations) -> None:
+
+    # TODO: enhance get_all_users to log name of the file
     users = file_reader.get_all_users(csv_path)
     upload_file(db_adapter, users, Table.USER)
+
+
+# TODO: add more functions like _load_sections(...) here. And delete this
+# comment once that pattern has been followed.
 
 
 def run_loader(arguments: MainArguments) -> None:
     logger.info("Begin loading files into the LMS Data Store (DS)...")
 
-    db_engine = create_engine(arguments.connection_string)
-    migrate(db_engine)
+    migrate(arguments.get_db_engine())
 
     csv_path = arguments.csv_path
     db_adapter = arguments.get_db_operations_adapter()
