c2223eb775dad9633303389e51730640361029ac
Stephen A. Fuqua <stephen@safnet.com>
2021-03-15 17:40:43 -0500

    Assignment.AssignmentCategory is now optional

--- docs/supporting-artifacts/0030-LMS-ForeignKeys.sql
+++ docs/supporting-artifacts/0030-LMS-ForeignKeys.sql
@@ -1,3 +1,8 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
 ALTER TABLE [lms].[Assignment] WITH CHECK ADD CONSTRAINT [FK_Assignment_LMSSection] FOREIGN KEY ([LMSSectionIdentifier])
 REFERENCES [lms].[LMSSection] ([LMSSectionIdentifier])
 GO
