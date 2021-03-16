e8a4214c0de1ed362dea0a36580a8c025da62314
Stephen Fuqua <stephenfuqua@users.noreply.github.com>
2021-03-16 09:03:17 -0500

    Update src/schoology-extractor/tests/data_generation/__main__.py

--- src/schoology-extractor/tests/data_generation/__main__.py
+++ src/schoology-extractor/tests/data_generation/__main__.py
@@ -17,7 +17,7 @@ from tests.data_generation.discussions import (
 )
 from tests.data_generation.assignments import (
     generate_and_load_assignments,
-    generate_extra_assignments_without_enrollments,
+   # generate_extra_assignments_without_enrollments,
     rollback_loaded_assignments,
 )
 from tests.data_generation.enrollments import (
