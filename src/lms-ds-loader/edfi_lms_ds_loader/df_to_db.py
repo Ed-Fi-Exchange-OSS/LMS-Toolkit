3cc491e9232ce60f62293388170978ba3bc47cc6
Stephen A. Fuqua <stephen@safnet.com>
2021-03-16 15:06:04 -0500

    Functions for uploading assignments and assignment submission types

--- src/lms-ds-loader/edfi_lms_ds_loader/df_to_db.py
+++ src/lms-ds-loader/edfi_lms_ds_loader/df_to_db.py
@@ -7,14 +7,16 @@ import logging
 
 import pandas as pd
 
+from edfi_lms_ds_loader.helpers.constants import Table
 from edfi_lms_ds_loader.mssql_lms_operations import MssqlLmsOperations
+from edfi_lms_ds_loader.helpers import assignment_splitter
 
 logger = logging.getLogger(__name__)
 
 
 def upload_file(db_adapter: MssqlLmsOperations, df: pd.DataFrame, table: str) -> None:
     """
-    Uploads a DataFrame to the designated LMS table
+    Uploads a DataFrame to the designated LMS table.
 
     Parameters
     ----------
@@ -43,3 +45,52 @@ def upload_file(db_adapter: MssqlLmsOperations, df: pd.DataFrame, table: str) ->
     db_adapter.enable_staging_natural_key_index(table)
 
     logger.info(f"Done with {table} file.")
+
+
+def _upload_assignment_submission_types(
+    db_adapter: MssqlLmsOperations, submission_types_df: pd.DataFrame
+) -> None:
+    table = Table.ASSIGNMENT_SUBMISSION_TYPES
+
+    logger.info(f"Uploading {table} file ...")
+
+    db_adapter.disable_staging_natural_key_index(table)
+    db_adapter.truncate_staging_table(table)
+    db_adapter.insert_into_staging(submission_types_df, table)
+
+    db_adapter.insert_new_submission_types()
+    db_adapter.soft_delete_removed_submission_types()
+
+    db_adapter.enable_staging_natural_key_index(table)
+
+    logger.info(f"Done with {table} file.")
+
+
+def upload_assignments(
+    db_adapter: MssqlLmsOperations, assignments_df: pd.DataFrame
+) -> None:
+    """
+    Uploads an Assignments DataFrame to the Assignment and AssignmentSubmissionType
+    tables. The specialized function is required in order to work with the
+    collection table, AssignmentSubmissionType
+
+
+    Parameters
+    ----------
+    db_adapter: MssqlLmsOperations
+        Database engine-specific adapter/wrapper for database operations.
+    df: pd.DataFrame
+        A DataFrame to upload.
+    """
+
+    assignments_df, submissions_type_df = assignment_splitter.split(assignments_df)
+
+    # Truncate AssignmentDescription to max 1024 characters, matching the database
+    assignments_df["AssignmentDescription"] = assignments_df["AssignmentDescription"].str[:1024]  # type: ignore
+
+    # Upload the Assignments without SubmissionType
+    upload_file(db_adapter, assignments_df, Table.ASSIGNMENT)
+
+    # Now run a specialized upload for Assignment Submission Types
+    # Table.
+    _upload_assignment_submission_types(db_adapter, submissions_type_df)
--- src/lms-ds-loader/tests/test_df_to_db.py
+++ src/lms-ds-loader/tests/test_df_to_db.py
@@ -9,10 +9,9 @@ from unittest.mock import Mock
 import pandas as pd
 import pytest
 
-from edfi_lms_ds_loader.df_to_db import upload_file
+from edfi_lms_ds_loader.helpers.constants import Table
+from edfi_lms_ds_loader import df_to_db
 
-
-GENERIC_TABLE = "c"
 SOURCE_SYSTEM = "google"
 
 
@@ -20,40 +19,128 @@ def describe_given_a_dataframe() -> None:
     @pytest.fixture
     def when_uploading_it() -> Tuple[Mock, pd.DataFrame]:
         # Arrange
-        adapter = Mock()
+        adapter_mock = Mock()
 
         df = pd.DataFrame([{"SourceSystem": SOURCE_SYSTEM}])
 
         # Act
-        upload_file(adapter, df, GENERIC_TABLE)
+        df_to_db.upload_file(adapter_mock, df, Table.USER)
 
-        return adapter, df
+        return adapter_mock, df
 
     def it_disables_the_natural_key_index(when_uploading_it) -> None:
-        adapter, _ = when_uploading_it
-        adapter.disable_staging_natural_key_index.assert_called_with(GENERIC_TABLE)
+        adapter_mock, _ = when_uploading_it
+        adapter_mock.disable_staging_natural_key_index.assert_called_with(Table.USER)
 
     def it_truncates_the_staging_table(when_uploading_it) -> None:
-        adapter, _ = when_uploading_it
-        adapter.truncate_staging_table.assert_called_with(GENERIC_TABLE)
+        adapter_mock, _ = when_uploading_it
+        adapter_mock.truncate_staging_table.assert_called_with(Table.USER)
 
     def it_reenables_natural_key_index(when_uploading_it) -> None:
-        adapter, _ = when_uploading_it
-        adapter.enable_staging_natural_key_index.assert_called_with(GENERIC_TABLE)
+        adapter_mock, _ = when_uploading_it
+        adapter_mock.enable_staging_natural_key_index.assert_called_with(Table.USER)
 
     def it_inserts_into_staging_table(when_uploading_it) -> None:
-        adapter, df = when_uploading_it
-        adapter.insert_into_staging.assert_called_with(df, GENERIC_TABLE)
+        adapter_mock, df = when_uploading_it
+        adapter_mock.insert_into_staging.assert_called_with(df, Table.USER)
 
     def it_inserts_into_production_table(when_uploading_it) -> None:
-        adapter, _ = when_uploading_it
-        adapter.insert_new_records_to_production.assert_called_with(GENERIC_TABLE, ["SourceSystem"])
+        adapter_mock, _ = when_uploading_it
+        adapter_mock.insert_new_records_to_production.assert_called_with(
+            Table.USER, ["SourceSystem"]
+        )
 
     def it_updates_production_table(when_uploading_it) -> None:
-        adapter, _ = when_uploading_it
-        adapter.copy_updates_to_production.assert_called_with(GENERIC_TABLE, ["SourceSystem"])
+        adapter_mock, _ = when_uploading_it
+        adapter_mock.copy_updates_to_production.assert_called_with(
+            Table.USER, ["SourceSystem"]
+        )
 
     def it_soft_deletes_from_production_table(when_uploading_it) -> None:
-        adapter, _ = when_uploading_it
-        adapter.soft_delete_from_production.assert_called_with(GENERIC_TABLE, SOURCE_SYSTEM)
+        adapter_mock, _ = when_uploading_it
+        adapter_mock.soft_delete_from_production.assert_called_with(
+            Table.USER, SOURCE_SYSTEM
+        )
 
+
+def describe_given_assignment_submission_types() -> None:
+    @pytest.fixture
+    def when_uploading_it(mocker) -> Tuple[Mock, Mock, pd.DataFrame, pd.DataFrame]:
+        # Arrange
+        adapter_mock = Mock()
+
+        description = "".join(["1"] * 1025)
+
+        assignments_df = pd.DataFrame(
+            [{"SourceSystem": SOURCE_SYSTEM, "AssignmentDescription": description}]
+        )
+        submissions_df = pd.DataFrame([{"SubmissionType": "whatever"}])
+        response = (assignments_df, submissions_df)
+        mocker.patch(
+            "edfi_lms_ds_loader.helpers.assignment_splitter.split",
+            return_value=response,
+        )
+
+        # Mock the regular upload process, since it was tested already up above
+        assignment_upload_mock = mocker.patch("edfi_lms_ds_loader.df_to_db.upload_file")
+
+        # Act
+        df_to_db.upload_assignments(adapter_mock, assignments_df)
+
+        return adapter_mock, assignment_upload_mock, assignments_df, submissions_df
+
+    def it_uploads_assignments(mocker, when_uploading_it) -> None:
+        adapter_mock, assignment_upload_mock, assignments_df, _ = when_uploading_it
+
+        assignment_upload_mock.assert_called_with(
+            adapter_mock, assignments_df, Table.ASSIGNMENT
+        )
+
+    def it_disables_submission_type_natural_key(mocker, when_uploading_it) -> None:
+        adapter_mock, _, _, _ = when_uploading_it
+
+        adapter_mock.disable_staging_natural_key_index.assert_called_with(
+            Table.ASSIGNMENT_SUBMISSION_TYPES
+        )
+
+    def it_truncates_AssignmentDescription_to_1024_chars(
+        mocker, when_uploading_it
+    ) -> None:
+        _, _, assignment_df, _ = when_uploading_it
+
+        assert len(assignment_df.iloc[0]["AssignmentDescription"]) == 1024
+
+    def it_truncates_submission_types_staging_table(mocker, when_uploading_it) -> None:
+        adapter_mock, _, _, submissions_df = when_uploading_it
+
+        adapter_mock.truncate_staging_table.assert_called_with(
+            Table.ASSIGNMENT_SUBMISSION_TYPES
+        )
+
+    def it_inserts_submission_types_into_staging(mocker, when_uploading_it) -> None:
+        adapter_mock, _, _, submissions_df = when_uploading_it
+
+        adapter_mock.insert_into_staging.assert_called_with(
+            submissions_df, Table.ASSIGNMENT_SUBMISSION_TYPES
+        )
+
+    def it_inserts_submission_types_into_production_table(
+        mocker, when_uploading_it
+    ) -> None:
+        adapter_mock, _, _, _ = when_uploading_it
+
+        adapter_mock.insert_new_submission_types.assert_called_once()
+
+    def it_soft_deletes_submission_types_in_production_table(
+        mocker, when_uploading_it
+    ) -> None:
+        adapter_mock, _, _, _ = when_uploading_it
+
+        adapter_mock.soft_delete_removed_submission_types.assert_called_once()
+
+    def it_reenables_submission_type_natural_key(mocker, when_uploading_it) -> None:
+        adapter_mock, _, _, _ = when_uploading_it
+
+        adapter_mock.enable_staging_natural_key_index.assert_called_with(
+            Table.ASSIGNMENT_SUBMISSION_TYPES
+        )
