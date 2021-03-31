09565e2bcc8aecaa3b5d0aeb3cf928b91948a590
Stephen A. Fuqua <stephen@safnet.com>
2021-03-31 10:40:19 -0500

    Refactor handling of initial database setup to avoid causing an exception

--- src/lms-ds-loader/edfi_lms_ds_loader/migrator.py
+++ src/lms-ds-loader/edfi_lms_ds_loader/migrator.py
@@ -19,12 +19,11 @@ logger = logging.getLogger(__name__)
 MIGRATION_SCRIPTS = [
     # CAUTION: these scripts will run in order from "top to bottom", so it is
     # critical to maintain the script order at all times.
-    "initialize_lms_database",
     "create_user_tables",
     "create_section_tables",
     "create_assignment_tables",
     "create_section_association_tables",
-    "create_processed_files_table"
+    "create_processed_files_table",
 ]
 
 
@@ -69,26 +68,56 @@ def _record_migration_in_journal(engine: sa_Engine, migration: str) -> None:
     execute_statements(engine, [statement])
 
 
+def _lms_schema_exists(engine: sa_Engine) -> bool:
+    statement = """
+select case when exists (
+    select 1 from INFORMATION_SCHEMA.SCHEMATA where schema_name = 'lms'
+) then 1 else 0 end
+""".strip()
+
+    return get_int(engine, statement) == 1
+
+
+def _run_migration_script(engine: sa_Engine, migration: str) -> None:
+
+    logger.debug(f"Running migration {migration}...")
+
+    migration_script = _get_script_path(engine, f"{migration}.sql")
+
+    statements = _read_statements_from_file(migration_script)
+    execute_statements(engine, statements)
+
+    _record_migration_in_journal(engine, migration)
+
+    logger.debug(f"Done with migration {migration}.")
+
+
 def migrate(engine: sa_Engine) -> None:
+    """
+    Runs database migration scripts for installing LMS table schema into the
+    destination database.
+
+    Parameters
+    ----------
+    engine: sa_Engine
+        SQL Alchemy database engine object.
+    """
     logger.info("Begin database auto-migration...")
 
+    if not _lms_schema_exists(engine):
+        _run_migration_script(engine, "initialize_lms_database")
+
     for migration in MIGRATION_SCRIPTS:
+        # The following block of code does not belong in _run_migration_script
+        # because it will throw an exception if the migration journal does not
+        # exist, and therefore is not appropriate when initializing the LMS
+        # database.
         if _script_has_been_run(engine, migration):
             logger.debug(
                 f"Migration {migration} has already run and will not be re-run."
             )
             continue
 
-        logger.debug(f"Running migration {migration}...")
-
-        script_name = f"{migration}.sql"
-        migration_script = _get_script_path(engine, script_name)
-
-        statements = _read_statements_from_file(migration_script)
-        execute_statements(engine, statements)
-
-        _record_migration_in_journal(engine, migration)
-
-        logger.debug(f"Done with migration {migration}.")
+        _run_migration_script(engine, migration)
 
     logger.info("Done with database auto-migration.")
