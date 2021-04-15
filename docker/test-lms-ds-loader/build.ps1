2d918d01dfcce7c867bd9a96017662f42228de30
Stephen A. Fuqua <stephen@safnet.com>
2021-04-15 13:42:06 -0500

    Exclude .env file

--- docker/test-lms-ds-loader/build.ps1
+++ docker/test-lms-ds-loader/build.ps1
@@ -2,7 +2,7 @@
 # that they can be copied into the Docker image.
 $tmp = New-Item -Type Directory -Path tmp -Force
 
-Copy-Item -Recurse -Path "../../src/lms-ds-loader" -Destination $tmp -Force
+Copy-Item -Recurse -Path "../../src/lms-ds-loader" -Destination $tmp -Force -Exclude ".env"
 Copy-Item -Recurse -Path "../../docs/sample-out" -Destination $tmp -Force
 
 # Now we can build the image
