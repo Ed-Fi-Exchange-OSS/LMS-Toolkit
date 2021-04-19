d5632a9afa3d53a87be4316c6251cb90ca193e82
Stephen A. Fuqua <stephen@safnet.com>
2021-04-19 10:26:54 -0500

    Tweaking directories

--- docker/test-lms-ds-loader/prep.sh
+++ docker/test-lms-ds-loader/prep.sh
@@ -4,12 +4,14 @@
 # See the LICENSE and NOTICES files in the project root for more information.
 
 # Copies files into a temporary diretory so that the Dockerfile can reference
-# them without ascending beyond the parent directory.
+# them without ascending beyond the parent directory. Note that the directory
+# paths are carefully crafted to work when run from within a GitHub action,
+# rather than running from a workstation plain Bash prompt
 
-mkdir -p tmp
+mkdir -p docker/test-lms-ds-loader/tmp/
 
-cp -rf ./src/lms-ds-loader/edfi_lms_ds_loader docker/test-lms-ds-loader/tmp/
-cp -rf ./src/lms-ds-loader/pyproject.toml docker/test-lms-ds-loader/tmp/
-cp -rf ./src/lms-ds-loader/poetry.lock docker/test-lms-ds-loader/tmp/
-cp -rf ./src/lms-ds-loader/README.md docker/test-lms-ds-loader/tmp/
-cp -rf ./docs/sample-out docker/test-lms-ds-loader/tmp/
+cp -r ./src/lms-ds-loader/edfi_lms_ds_loader docker/test-lms-ds-loader/tmp/edfi_lms_ds_loader
+cp ./src/lms-ds-loader/pyproject.toml docker/test-lms-ds-loader/tmp/
+cp ./src/lms-ds-loader/poetry.lock docker/test-lms-ds-loader/tmp/
+cp ./src/lms-ds-loader/README.md docker/test-lms-ds-loader/tmp/
+cp ./docs/sample-out docker/test-lms-ds-loader/tmp/
