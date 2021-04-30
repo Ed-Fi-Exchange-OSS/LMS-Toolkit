c9d8b54c0628d18d5bd9c52e7ba5cbe643d0d398
Stephen A. Fuqua <stephen@safnet.com>
2021-04-30 13:57:53 -0500

    Refactor extract facade for proper exception handling

--- src/schoology-extractor/edfi_schoology_extractor/extract_facade.py
+++ src/schoology-extractor/edfi_schoology_extractor/extract_facade.py
@@ -18,15 +18,24 @@ from edfi_schoology_extractor import usage_analytics_facade
 import edfi_schoology_extractor.lms_filesystem as lms
 from edfi_schoology_extractor.helpers.sync import get_sync_db_engine
 from edfi_schoology_extractor.client_facade import ClientFacade
+from edfi_lms_extractor_lib.helpers.decorators import catch_exceptions
 
 logger = logging.getLogger(__name__)
 
+# This variable facilitates temporary storage of output results from one GET
+# request that need to be used for creating another GET request.
+result_bucket: Dict[str, DataFrame] = {}
 
-def _initialize(arguments: MainArguments) -> Tuple[ClientFacade, sqlalchemy.engine.base.Engine]:
+
+def _initialize(
+    arguments: MainArguments,
+) -> Tuple[ClientFacade, sqlalchemy.engine.base.Engine]:
     global logger
 
     try:
-        request_client: RequestClient = RequestClient(arguments.client_key, arguments.client_secret)
+        request_client: RequestClient = RequestClient(
+            arguments.client_key, arguments.client_secret
+        )
         db_engine = get_sync_db_engine(arguments.sync_database_directory)
 
         facade = ClientFacade(request_client, arguments.page_size, db_engine)
@@ -41,94 +50,137 @@ def _initialize(arguments: MainArguments) -> Tuple[ClientFacade, sqlalchemy.engi
         sys.exit(1)
 
 
-def _create_file_from_dataframe(df: Optional[DataFrame], file_name) -> bool:
-    global logger
-
+def _create_file_from_dataframe(df: Optional[DataFrame], file_name) -> None:
     normalized_file_name = os.path.normpath(file_name)
     logger.info(f"Exporting {normalized_file_name}")
-    try:
-        if df is not None:
-            csv_writer.df_to_csv(df, normalized_file_name)
-        else:
-            csv_writer.df_to_csv(DataFrame(), normalized_file_name)
 
-        return True
-    except BaseException:
-        logger.exception(
-            "An exception occurred while generating %s", normalized_file_name
+    if df is not None:
+        csv_writer.df_to_csv(df, normalized_file_name)
+    else:
+        csv_writer.df_to_csv(DataFrame(), normalized_file_name)
+
+
+@catch_exceptions
+def _get_users(client_facade: ClientFacade, output_directory: str) -> None:
+    users = client_facade.get_users()
+
+    _create_file_from_dataframe(users, lms.get_user_file_path(output_directory))
+
+
+@catch_exceptions
+def _get_sections(client_facade: ClientFacade, output_directory: str) -> None:
+
+    sections = client_facade.get_sections()
+    result_bucket["sections"] = sections
+
+    _create_file_from_dataframe(
+        sections, lms.get_section_file_path(output_directory)
+    )
+
+
+@catch_exceptions
+def _get_assignments(
+    client_facade: ClientFacade, output_directory: str, section_id: int
+) -> None:
+
+    assignment_file_path: str = lms.get_assignment_file_path(
+        output_directory, section_id
+    )
+
+    assignments = client_facade.get_assignments(section_id)
+    result_bucket["assignments"] = assignments
+
+    _create_file_from_dataframe(assignments, assignment_file_path)
+
+
+@catch_exceptions
+def _get_section_activities(
+    facade: ClientFacade, output_directory: str, section_id: int
+) -> None:
+
+    section_activities_file_path: str = lms.get_section_activities_file_path(
+        output_directory, section_id
+    )
+    section_activities = facade.get_section_activities(section_id)
+
+    _create_file_from_dataframe(section_activities, section_activities_file_path)
+
+
+@catch_exceptions
+def _get_submissions(
+    client_facade: ClientFacade, output_directory: str, section_id: int
+) -> None:
+    assignments: DataFrame = result_bucket["assignments"]
+
+    if not assignments.empty:
+        for assignment in assignments["SourceSystemIdentifier"].tolist():
+
+            assignment_id = int(assignment)
+            submission_file_name = lms.get_submissions_file_path(
+                output_directory, section_id, assignment_id
+            )
+
+            submissions = client_facade.get_submissions(assignment_id, section_id)
+            _create_file_from_dataframe(
+                submissions,
+                submission_file_name,
+            )
+
+
+@catch_exceptions
+def _get_section_associations(
+    client_facade: ClientFacade, output_directory: str, section_id: int
+) -> None:
+    file_path = lms.get_section_association_file_path(output_directory, section_id)
+
+    section_associations = client_facade.get_section_associations(section_id)
+    result_bucket["section_associations"] = section_associations
+
+    _create_file_from_dataframe(section_associations, file_path)
+
+
+@catch_exceptions
+def _get_attendance_events(
+    client_facade: ClientFacade, output_directory: str, section_id: int
+) -> None:
+    file_path = lms.get_attendance_events_file_path(output_directory, section_id)
+
+    section_associations: DataFrame = result_bucket["section_associations"]
+
+    attendance_events: DataFrame = client_facade.get_attendance_events(
+        section_id, section_associations
+    )
+
+    _create_file_from_dataframe(attendance_events, file_path)
+
+
+@catch_exceptions
+def _get_system_activities(
+    arguments: MainArguments, db_engine: sqlalchemy.engine.base.Engine
+) -> None:
+    need_to_process_input_files = arguments.input_directory is not None
+
+    if need_to_process_input_files:
+        system_activities_output_dir: str = lms.get_system_activities_file_path(
+            arguments.output_directory
         )
-        return False
 
+        system_activities = usage_analytics_facade.get_system_activities(
+            arguments.input_directory, db_engine
+        )
 
-def _get_users(extractorFacade: ClientFacade) -> DataFrame:
-    return extractorFacade.get_users()
-
-
-# This variable facilitates temporary storage of output results from one GET
-# request that need to be used for creating another GET request.
-result_bucket: Dict[str, DataFrame] = {}
-
-
-def _get_sections(extractorFacade: ClientFacade) -> DataFrame:
-    sections_df: DataFrame = extractorFacade.get_sections()
-    result_bucket["sections"] = sections_df
-    return sections_df
-
-
-def _get_assignments(extractorFacade: ClientFacade, section_id: int) -> Optional[DataFrame]:
-    assignments_df: DataFrame = extractorFacade.get_assignments(section_id)
-    result_bucket["assignments"] = assignments_df
-    return assignments_df
-
-
-def _get_section_activities(extractorFacade: ClientFacade, section_id: int) -> Optional[DataFrame]:
-    section_activities_df: DataFrame = extractorFacade.get_section_activities(
-        section_id
-    )
-    result_bucket["section_activities"] = section_activities_df
-
-    return section_activities_df
-
-
-def _get_submissions(extractorFacade: ClientFacade, assignment_id: int, section_id: int) -> Optional[DataFrame]:
-    return extractorFacade.get_submissions(assignment_id, section_id)
-
-
-def _get_section_associations(extractorFacade: ClientFacade, section_id: int) -> DataFrame:
-    section_associations_df: DataFrame = extractorFacade.get_section_associations(
-        section_id
-    )
-    result_bucket["section_associations"] = section_associations_df
-
-    return section_associations_df
-
-
-def _get_attendance_events(extractorFacade: ClientFacade, section_id: int) -> DataFrame:
-    section_associations_df: DataFrame = result_bucket["section_associations"]
-    attendance_events_df: DataFrame = extractorFacade.get_attendance_events(
-        section_id, section_associations_df
-    )
-
-    return attendance_events_df
-
-
-def _get_system_activities(input_directory: str, db_engine: sqlalchemy.engine.base.Engine) -> DataFrame:
-    return usage_analytics_facade.get_system_activities(input_directory, db_engine)
+        _create_file_from_dataframe(system_activities, system_activities_output_dir)
 
 
 def run(arguments: MainArguments) -> None:
     logger.info("Starting Ed-Fi LMS Schoology Extractor")
     facade, db_engine = _initialize(arguments)
 
-    _create_file_from_dataframe(
-        _get_users(facade), lms.get_user_file_path(arguments.output_directory)
-    )
-    succeeded = _create_file_from_dataframe(
-        _get_sections(facade), lms.get_section_file_path(arguments.output_directory)
-    )
+    _get_users(facade, arguments.output_directory)
 
-    # Cannot proceed with section-related files if the sections extract did not
-    # succeed.
+    _get_sections(facade, arguments.output_directory)
+    succeeded = result_bucket["sections"] is not None
+
     if not succeeded:
         logger.critical(
             "Unable to continue file generation because the load of Sections failed. Please review the log for more information."
@@ -137,54 +189,16 @@ def run(arguments: MainArguments) -> None:
 
     for section_id in result_bucket["sections"]["SourceSystemIdentifier"].values:
 
-        assignment_file_path: str = lms.get_assignment_file_path(
-            arguments.output_directory, section_id
-        )
-        succeeded = _create_file_from_dataframe(
-            _get_assignments(facade, section_id), assignment_file_path
-        )
+        _get_assignments(facade, arguments.output_directory, section_id)
+        succeeded = result_bucket["assignments"] is not None
 
         if succeeded:
-            assignments_df: DataFrame = result_bucket["assignments"]
+            _get_submissions(facade, arguments.output_directory, section_id)
 
-            if not assignments_df.empty:
-                for assignment in assignments_df["SourceSystemIdentifier"].tolist():
+        _get_section_activities(facade, arguments.output_directory, section_id)
+        _get_section_associations(facade, arguments.output_directory, section_id)
+        _get_attendance_events(facade, arguments.output_directory, section_id)
 
-                    assignment_id: int = int(assignment)
-                    submission_file_name: str = lms.get_submissions_file_path(
-                        arguments.output_directory, section_id, assignment_id
-                    )
-                    _create_file_from_dataframe(
-                        _get_submissions(facade, assignment_id, section_id),
-                        submission_file_name,
-                    )
-
-        section_activities_file_path: str = lms.get_section_activities_file_path(
-            arguments.output_directory, section_id
-        )
-        _create_file_from_dataframe(
-            _get_section_activities(facade, section_id), section_activities_file_path
-        )
-
-        file_path = lms.get_section_association_file_path(
-            arguments.output_directory, section_id
-        )
-        succeeded = _create_file_from_dataframe(
-            _get_section_associations(facade, section_id), file_path
-        )
-
-        file_path = lms.get_attendance_events_file_path(
-            arguments.output_directory, section_id
-        )
-        _create_file_from_dataframe(_get_attendance_events(facade, section_id), file_path)
-
-    need_to_process_input_files = arguments.input_directory is not None
-    if need_to_process_input_files:
-        system_activities_output_dir: str = lms.get_system_activities_file_path(
-            arguments.output_directory
-        )
-        _create_file_from_dataframe(
-            _get_system_activities(arguments.input_directory, db_engine), system_activities_output_dir
-        )
+    _get_system_activities(arguments, db_engine)
 
     logger.info("Finishing Ed-Fi LMS Schoology Extractor")
