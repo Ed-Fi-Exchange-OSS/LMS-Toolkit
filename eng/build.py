45b167333355d113f663727b50f7c5eb0f2ea9e8
Stephen Fuqua <stephenfuqua@users.noreply.github.com>
2021-06-02 15:16:39 -0500

    [LMS-301] New build configurations (#197)
    * Rename files
    
    * Build configurations for file-utils library
    
    * Build configurations for extractor-lib
    
    * Build configurations for the lms-ds-loader
    
    * Remove extra file
    
    * Canvas: fix dependency caching and improve re-useability (#198)
    
    * Copy canvas build improvements to other projects
    
    * Support manual execution
    
    * Account for slash difference on windows
    
    * Refactor build script to support CI actions, and refactor the Canvas CI jobs to use the new build script tasks
    
    * Restore hard-coding to 3.9.4
    
    * Reformat the build script
    
    * Setup proper caching when publishing
    
    * Forgot about the env vars
    
    * correction
    
    * Copy improvements from canvas
    
    * Restore 3.9.x
    
    * Copy build script improvements

--- eng/build.py
+++ eng/build.py
@@ -6,7 +6,7 @@
 """Build automation scripts"""
 
 import os
-import subprocess
+from subprocess import run
 import shutil
 import sys
 from typing import List
@@ -25,6 +25,8 @@ def _display_help():
 * typecheck:xml
 * build
 * publish
+* ci:test
+* ci:publish
 
 For example, to run unit tests with code coverage, optimized for TeamCity
 output, on utility `lms-ds-loader`:
@@ -37,13 +39,13 @@ output, on utility `lms-ds-loader`:
 
 def _run_command(command: List[str], exit_immediately: bool = True):
 
-    print('\033[95m' + " ".join(command) + '\033[0m')
+    print("\033[95m" + " ".join(command) + "\033[0m")
 
     # Some system configurations on Windows-based CI servers have trouble
     # finding poetry, others do not. Explicitly calling "cmd /c" seems to help,
     # though unsure why.
 
-    if (os.name == "nt"):
+    if os.name == "nt":
         # All versions of Windows are "nt"
         command = ["cmd", "/c", *command]
 
@@ -58,138 +60,141 @@ def _run_command(command: List[str], exit_immediately: bool = True):
         if not os.path.exists(package_dir):
             raise RuntimeError(f"Cannot find package {package_name}")
 
-    result = subprocess.run(command, cwd=package_dir)
+    result = run(command, cwd=package_dir)
+
+    return_code = result.returncode
 
     if exit_immediately:
-        exit(result.returncode)
+        # Exits the script regardless of the prior return code
+        exit(return_code)
 
-    if result.returncode != 0:
-        exit(result.returncode)
-
-
-def _run_install():
-    _run_command([
-        "poetry",
-        "install"
-    ])
+    if return_code != 0:
+        # Only exits the script for non-zero return code
+        exit(return_code)
 
 
-def _run_tests():
-    _run_command([
-        "poetry",
-        "run",
-        "pytest",
-        "tests",
-    ])
+def _run_install(exit_immediately: bool = True):
+    _run_command(["poetry", "install"], exit_immediately)
+
+
+def _run_tests(exit_immediately: bool = True):
+    _run_command(
+        [
+            "poetry",
+            "run",
+            "pytest",
+            "tests",
+        ],
+        exit_immediately,
+    )
+
+
+def _run_coverage_without_report():
+    _run_command(
+        [
+            "poetry",
+            "run",
+            "coverage",
+            "run",
+            "-m",
+            "pytest",
+            "tests",
+        ],
+        exit_immediately=True,
+    )
 
 
 def _run_coverage():
-    _run_command([
-        "poetry",
-        "run",
-        "coverage",
-        "run",
-        "-m",
-        "pytest",
-        "tests",
-    ], exit_immediately=False)
-    _run_command([
-        "poetry",
-        "run",
-        "coverage",
-        "report",
-    ], exit_immediately=False)
+    _run_coverage_without_report()
+
+    _run_command(
+        [
+            "poetry",
+            "run",
+            "coverage",
+            "report",
+        ],
+        exit_immediately=True,
+    )
 
 
-def _run_coverage_html():
-    _run_command([
-        "poetry",
-        "run",
-        "coverage",
-        "run",
-        "-m",
-        "pytest",
-        "tests",
-    ], exit_immediately=False)
-    _run_command([
-        "poetry",
-        "run",
-        "coverage",
-        "html",
-    ], exit_immediately=False)
+def _run_coverage_html(exit_immediately: bool = True):
+    _run_coverage_without_report()
+
+    _run_command(
+        [
+            "poetry",
+            "run",
+            "coverage",
+            "html",
+        ],
+        exit_immediately,
+    )
 
 
 def _run_coverage_xml():
-    _run_command([
-        "poetry",
-        "run",
-        "coverage",
-        "run",
-        "-m",
-        "pytest",
-        "tests",
-    ], exit_immediately=False)
-    _run_command([
-        "poetry",
-        "run",
-        "coverage",
-        "xml"
-    ])
+    _run_command(
+        [
+            "poetry",
+            "run",
+            "coverage",
+            "run",
+            "-m",
+            "pytest",
+            "tests",
+        ],
+        exit_immediately=False,
+    )
+
+    _run_command(["poetry", "run", "coverage", "xml"], exit_immediately=False)
 
 
-def _run_lint():
-    _run_command([
-        "poetry",
-        "run",
-        "flake8"
-    ])
+def _run_lint(exit_immediately: bool = True):
+    _run_command(["poetry", "run", "flake8"], exit_immediately)
 
 
 def _run_typecheck():
-    _run_command([
-        "poetry",
-        "run",
-        "mypy"
-    ])
+    _run_command(["poetry", "run", "mypy"], exit_immediately=True)
 
 
-def _run_typecheck_xml():
-    _run_command([
-        "poetry",
-        "run",
-        "mypy",
-        "--junit-xml",
-        "mypy.xml"
-    ])
+def _run_typecheck_xml(exit_immediately: bool = True):
+    _run_command(["poetry", "run", "mypy", "--junit-xml", "mypy.xml"], exit_immediately)
 
 
-def _run_build():
-    _run_command([
-        "poetry",
-        "build"
-    ])
+def _run_build(exit_immediately: bool = True):
+    _run_command(["poetry", "build"], exit_immediately)
 
 
-def _run_publish():
-    shutil.rmtree('dist', ignore_errors=True)
+def _run_publish(exit_immediately: bool = True):
+    shutil.rmtree("dist", ignore_errors=True)
 
-    _run_command([
-        "poetry",
-        "build"
-    ], exit_immediately=False)
+    _run_build(False)
 
-    _run_command([
-        "poetry",
-        "run",
-        "twine",
-        "upload",
-        "dist/*"
-    ])
+    _run_command(["poetry", "run", "twine", "upload", "dist/*"], exit_immediately)
+
+
+def _run_ci_test():
+    """
+    Calls the commands required for a continuous integration testing job.
+    """
+    _run_install(False)
+    _run_coverage_html(False)
+    _run_typecheck_xml(False)
+    _run_lint(True)
+
+
+def _run_ci_publish():
+    """
+    Calls the commands required for a continuous integration publishing job.
+    """
+    _run_install(False)
+    _run_tests(False)
+    _run_publish(True)
 
 
 if __name__ == "__main__":
-    if not sys.version_info >= (3, 8):
-        print("This program requires Python 3.8 or newer.", file=sys.stderr)
+    if not sys.version_info >= (3, 9):
+        print("This program requires Python 3.9 or newer.", file=sys.stderr)
         exit(-1)
 
     if len(sys.argv) < 3:
@@ -206,6 +211,8 @@ if __name__ == "__main__":
         "typecheck:xml": _run_typecheck_xml,
         "build": _run_build,
         "publish": _run_publish,
+        "ci:test": _run_ci_test,
+        "ci:publish": _run_ci_publish,
     }
 
     switcher.get(sys.argv[1], _display_help)()
