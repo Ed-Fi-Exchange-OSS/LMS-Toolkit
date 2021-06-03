980ac255383ac93d1cd4991377dfb5ce9c631901
Brad Banister <brad@banisterconsult.com>
2021-06-03 16:07:26 -0500

    [LMS-305] endpoint of one-off delete utility

--- src/canvas-extractor/tests/data_generation/data_remover/__main__.py
+++ src/canvas-extractor/tests/data_generation/data_remover/__main__.py
@@ -9,7 +9,6 @@ import sys
 from dotenv import load_dotenv
 from canvasapi import Canvas
 from canvasapi.account import Account
-from edfi_canvas_extractor.api.api_caller import call_with_retry
 from edfi_canvas_extractor.helpers import arg_parser
 from edfi_canvas_extractor.config import get_canvas_api
 from edfi_lms_file_utils import file_reader
@@ -28,29 +27,35 @@ arguments = arg_parser.parse_main_arguments(sys.argv[1:])
 
 
 canvas: Canvas = get_canvas_api(arguments.base_url, arguments.access_token)
-account: Account = call_with_retry(canvas.get_accounts)[0]
+account: Account = canvas.get_accounts()[0]
 
-# remove sections
 sections_df = file_reader.get_all_sections(arguments.output_directory)
 section_ids = sections_df["SourceSystemIdentifier"]
+
 for section_id in section_ids:
     section_id_int: int = int(section_id)
-    if section_id_int > 500 and section_id_int < 756:
-        # delete
+    if section_id_int > 649 and section_id_int < 751:
         section_to_delete = canvas.get_section(section_id_int)
+        # enrollments = section_to_delete.get_enrollments()
+
+        # # delete enrollments
+        # for enrollment in enrollments:
+        #     enrollment.deactivate(task="delete")
+
+        # delete section
         section_to_delete.delete()
         logger.info("**** Deleted section with id %s", section_id_int)
 
 logger.info("**** Successfully deleted %s section", len(section_ids))
 
 # remove users
-users_df = file_reader.get_all_users(arguments.output_directory)
-user_ids = users_df["SourceSystemIdentifier"]
-for user_id in user_ids:
-    user_id_int: int = int(user_id)
-    if user_id_int > 10000 and user_id_int < 50000:
-        # delete
-        account.delete_user(user_id_int)
-        logger.info("**** Deleted user with id %s", user_id_int)
+# users_df = file_reader.get_all_users(arguments.output_directory)
+# user_ids = users_df["SourceSystemIdentifier"]
+# for user_id in user_ids:
+#     user_id_int: int = int(user_id)
+#     if user_id_int > 10000 and user_id_int < 50000:
+#         # delete
+#         account.delete_user(user_id_int)
+#         logger.info("**** Deleted user with id %s", user_id_int)
 
-logger.info("**** Successfully deleted %s users", len(user_ids))
+# logger.info("**** Successfully deleted %s users", len(user_ids))
