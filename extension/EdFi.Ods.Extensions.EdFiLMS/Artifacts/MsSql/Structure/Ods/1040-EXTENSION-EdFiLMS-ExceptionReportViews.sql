1f48493a47286706c248817e85b4c745de7cbc82
Stephen A. Fuqua <stephen@safnet.com>
2021-06-11 21:10:17 -0500

    Initial report views

---
+++ extension/EdFi.Ods.Extensions.EdFiLMS/Artifacts/MsSql/Structure/Ods/1040-EXTENSION-EdFiLMS-ExceptionReportViews.sql
@@ -0,0 +1,29 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+CREATE VIEW edfilms.exceptions_LMSSection AS
+    SELECT
+        *
+    FROM
+        lms.LMSSection
+    WHERE
+        EdFiSectionId IS NULL
+    AND
+        DeletedAt IS NULL;
+
+GO
+
+
+CREATE VIEW edfilms.exceptions_LMSUser AS
+    SELECT
+        *
+    FROM
+        lms.LMSUser
+    WHERE
+        EdFiStudentUniqueId IS NULL
+    AND
+        DeletedAt IS NULL;
+
+GO
