24131a06ef60878bbb61b66875213eb1286424e6
Stephen A. Fuqua <stephen@safnet.com>
2021-06-14 15:34:23 -0500

    exceptions report code needs testing

--- src/lms-harmonizer/edfi_lms_harmonizer/exceptions_reports.py
+++ src/lms-harmonizer/edfi_lms_harmonizer/exceptions_reports.py
@@ -3,40 +3,58 @@
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
 
+from datetime import datetime
+from logging import getLogger
+from os import path
+
 import pandas as pd
+from sqlalchemy.engine.base import Engine as sa_Engine
 
-# Scaffold only for now
+from edfi_sql_adapter import sql_adapter
 
-def _query_for_section_summary() -> str:
-    return """
+
+logger = getLogger(__name__)
+
+QUERY_FOR_SECTIONS = "SELECT * FROM edfilms.exceptions_LMSSection"
+QUERY_FOR_SECTION_SUMMARY = """
 SELECT
     COUNT(1) as UnmatchedCount
 FROM
     edfilms.exceptions_LMSSection
     """
-
-
-def _query_for_section_user() -> str:
-    return """
+QUERY_FOR_USERS = "SELECT * FROM edfilms.exceptions_LMSUser"
+QUERY_FOR_USERS_SUMMARY = """
 SELECT
     COUNT(1) as UnmatchedCount
 FROM
     edfilms.exceptions_LMSUser
     """
+SECTIONS = "sections"
+USERS = "users"
 
 
-def _read_from_database(query: str) -> pd.DataFrame:
-    return pd.read_sql(
-        
+def _get_file_path(output_directory: str, report_type: str) -> str:
+    return path.join(
+        output_directory, report_type, f"{datetime.now().strftime('%Y-%m-%d')}"
     )
 
 
-def get_summary() -> None:
+def get_summary(engine: sa_Engine) -> None:
+    sections_count = sql_adapter.get_int(engine, QUERY_FOR_SECTION_SUMMARY)
+    users_count = sql_adapter.get_int(engine, QUERY_FOR_USERS_SUMMARY)
+
+    if sections_count > 0 or users_count > 0:
+        logger.warn(
+            f"There are {sections_count} unmatched sections and {users_count} "
+            f"unmatched users in the database."
+        )
+    else:
+        logger.info("There are no unmatched sections or users in the database.")
 
 
+def create_files(engine: sa_Engine, output_directory: str) -> None:
+    sections = pd.read_sql(QUERY_FOR_SECTIONS, engine)
+    sections.to_csv(_get_file_path(output_directory, SECTIONS))
 
-    pass
-
-
-def create_files() -> None:
-    pass
+    users = pd.read_sql(QUERY_FOR_USERS, engine)
+    users.to_csv(_get_file_path(output_directory, USERS))
---
+++ src/lms-harmonizer/tests/test_exceptions_reports.py
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
+from edfi_lms_harmonizer import exceptions_reports
+
+def describe_when_getting_the_summary_report() -> None:
+
+    # Need to mock the sql_adapter to return expected counts
+    # and mock the logger
+
+    pass
