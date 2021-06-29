3fd03a3c98da3cd0cb52177c3126b9d1b812b2e8
Stephen A. Fuqua <stephen@safnet.com>
2021-06-29 16:45:03 -0500

    Simplify to "Google" as source system

--- src/google-classroom-extractor/edfi_google_classroom_extractor/mapping/constants.py
+++ src/google-classroom-extractor/edfi_google_classroom_extractor/mapping/constants.py
@@ -3,4 +3,4 @@
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
 
-SOURCE_SYSTEM = "Google Classroom"
+SOURCE_SYSTEM = "Google"
