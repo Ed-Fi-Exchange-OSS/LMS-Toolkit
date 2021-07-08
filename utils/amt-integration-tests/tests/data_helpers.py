bd76240829cde71db1c015ce99a5796b3d3dd9e3
Stephen A. Fuqua <stephen@safnet.com>
2021-07-08 17:00:21 -0500

    Re-arrange to have separate test files for each scenario, grouped by feature directory Various helpers for pre-loading data AssignmentDim testing is very much a work in progress

--- utils/amt-integration-tests/tests/data_helpers.py
+++ utils/amt-integration-tests/tests/data_helpers.py
@@ -6,40 +6,49 @@
 import os
 
 import pandas as pd
-from sqlalchemy import engine
+from sqlalchemy import engine, text
 
 DESCRIPTOR_SQL = """
 INSERT INTO edfi.Descriptor (CodeValue, ShortDescription, Description, Namespace)
-VALUES ('<cv>', '<sd>', '<d>', '<n>');
-INSERT INTO lmsx.<tbl> (<tbl>Id)
-VALUES (@@identity);
+    VALUES ('<cv>', '<sd>', '<d>', '<n>');
+DECLARE @id int = @@identity;
+INSERT INTO <schema>.<tbl> (<tbl>Id) VALUES (@id);
 """
+
+SCHEMA_LMSX = "lmsx"
+SCHEMA_EDFI = "edfi"
+
 
 def _get_value(row: pd.Series, column: str) -> str:
     # Escape any apostrophes so that this is safe to run
     return row[column].replace("'", "''")
 
 
-def _prepare_template(row: pd.Series, table: str) -> str:
+def _prepare_descriptor_sql(row: pd.Series, schema: str, table: str) -> str:
     return (
         DESCRIPTOR_SQL.replace("<cv>", _get_value(row, "CodeValue"))
-                      .replace("<sd>", _get_value(row, "ShortDescription"))
-                      .replace("<d>", _get_value(row, "Description"))
-                      .replace("<n>", _get_value(row, "Namespace"))
-                      .replace("<tbl>", table)
+        .replace("<sd>", _get_value(row, "ShortDescription"))
+        .replace("<d>", _get_value(row, "Description"))
+        .replace("<n>", _get_value(row, "Namespace"))
+        .replace("<tbl>", table)
+        .replace("<schema>", schema)
     )
 
 
-def _read_and_load_descriptors(engine: engine.base.Engine, descriptor_type: str) -> None:
+def _read_and_load_descriptors(
+    engine: engine.base.Engine, descriptor_type: str
+) -> None:
     descriptor = f"{descriptor_type}Descriptor"
 
-    file_path = os.path.join("..", "..", "extension", "Descriptors", f"{descriptor}.xml")
+    file_path = os.path.join(
+        "..", "..", "extension", "Descriptors", f"{descriptor}.xml"
+    )
     df = pd.read_xml(file_path)
 
     with engine.connect() as connection:
         for _, row in df.iterrows():
-            sql = _prepare_template(row, descriptor)
-            connection.execute(sql)
+            sql = _prepare_descriptor_sql(row, SCHEMA_LMSX, descriptor)
+            connection.execute(text(sql))
 
 
 def load_lms_descriptors(engine: engine.base.Engine) -> None:
@@ -47,3 +56,68 @@ def load_lms_descriptors(engine: engine.base.Engine) -> None:
     _read_and_load_descriptors(engine, "LMSSourceSystem")
     _read_and_load_descriptors(engine, "SubmissionStatus")
     _read_and_load_descriptors(engine, "SubmissionType")
+
+
+APPEND_OPTIONS = {"if_exists": "append", "index": False}
+
+
+def load_school(engine: engine.base.Engine, id: str) -> None:
+    ed_org = pd.DataFrame([{"EducationOrganizationId": id, "NameOfInstitution": id}])
+    ed_org.to_sql("EducationOrganization", engine, schema="edfi", **APPEND_OPTIONS)
+
+    school = pd.DataFrame([{"schoolid": id}])
+    school.to_sql("School", engine, schema="edfi", **APPEND_OPTIONS)
+
+
+def load_school_year(engine: engine.base.Engine, school_year: str) -> None:
+    school_year_type = pd.DataFrame(
+        [
+            {
+                "SchoolYear": school_year,
+                "SchoolYearDescription": school_year,
+                "CurrentSchoolYear": True,
+            }
+        ]
+    )
+    school_year_type = school_year_type.astype({"SchoolYear": int})
+    school_year_type.to_sql("SchoolYearType", engine, schema="edfi", **APPEND_OPTIONS)
+
+
+def load_session(
+    engine: engine.base.Engine, school_id: str, session_name: str, school_year: str
+) -> None:
+
+    # We need to have a term descriptor before we can insert a school year
+    term_code_value = f"TERM:{session_name}"
+    row = pd.Series(
+        {
+            "CodeValue": term_code_value,
+            "ShortDescription": session_name,
+            "Description": session_name,
+            "Namespace": "uri://ed-fi.org/Term",
+        }
+    )
+
+    term_descriptor_sql = _prepare_descriptor_sql(row, SCHEMA_EDFI, "TermDescriptor")
+    descriptor_id = 0
+
+    with engine.connect() as connection:
+        connection.execute(text(term_descriptor_sql))
+
+        sql = text(f"SELECT DescriptorId FROM edfi.Descriptor WHERE CodeValue = '{term_code_value}'")
+        result = connection.execute(sql, engine)
+        for row in result:
+            descriptor_id = row["DescriptorId"]
+
+    session = pd.DataFrame([{
+        "SchoolId": school_id,
+        "SchoolYear": school_year,
+        "SessionName": session_name,
+        "BeginDate": "2021-01-01",
+        "EndDate": "2021-08-03",
+        "TermDescriptorId": descriptor_id,
+        "TotalInstructionalDays": 1
+    }])
+
+    session.to_sql("Session", engine, schema=SCHEMA_EDFI, **APPEND_OPTIONS)
+
--- utils/amt-integration-tests/tests/features/assignmentdim_view.feature
+++ utils/amt-integration-tests/tests/features/assignmentdim_view.feature
@@ -27,3 +27,54 @@ Feature: AssignmentDim View
               | SectionKey       |
               | GradingPeriodKey |
               | LastModifiedDate |
+
+    Scenario: Ignores discussions
+        Given Analytics Middle Tier has been installed
+
+          And there is a school with id 54
+
+          And the school year is 2021
+
+          And school 54 has a session called "Fall" in year 2021
+
+         When I query for assignments with identifier "ignore-discussion"
+
+         Then there should be 0 records
+
+          # TODO: This is now done once for the entire session so that we
+          # don't have re-insertion attempts. # Let's discuss the value of having
+          # given statements like the one above or, possibly, the one below,
+          # that explain the conditions but don't actually do anything.
+
+        # And the default LMS descriptors have been loaded
+
+
+
+
+        #   And there is a section
+        #       | LocalCourseCode   | abc    |
+        #       | SchoolId          | 54     |
+        #       | Schoolyear        | 2021   |
+        #       | SectionIdentifier | qwerty |
+        #       | SessionName       | Fall   |
+
+        #   And there is a grading period
+        #       | Descriptor     | First summer session |
+        #       | PeriodSequence | 3                    |
+        #       | SchoolId       | 54                   |
+        #       | SchoolYear     | 2021                 |
+
+        #   And there is one Assignment
+        #       | AssignmentIdentifier | ignore-discussion |
+        #       | SchoolId             | 54                |
+        #       | SourceSystem         | Schoology         |
+        #       | Title                | A discussion      |
+        #       | Description          | The description   |
+        #       | StartDateTime        | 2021-07-08 09:00  |
+        #       | EndDateTime          | 2021-07-09 10:00  |
+        #       | DueDateTime          | 2021-07-09 10:11  |
+        #       | MaxPoints            | 99                |
+        #       | SectionIdentifier    | qwerty            |
+        #       | LastModifiedDate     | 2021-07-07 09:01  |
+        #       | AssignmentCategory   | discussion        |
+
