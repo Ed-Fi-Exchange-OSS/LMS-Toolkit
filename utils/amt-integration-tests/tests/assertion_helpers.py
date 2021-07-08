1ec000ac2529427a50fa9714c274d91cd06bc2af
Stephen A. Fuqua <stephen@safnet.com>
2021-07-08 13:23:02 -0500

    Move table helpers to a separate file

--- utils/amt-integration-tests/tests/assertion_helpers.py
+++ utils/amt-integration-tests/tests/assertion_helpers.py
@@ -3,29 +3,16 @@
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
 
-from typing import List, Tuple
 
 import pandas as pd
 
 
-def _read_string_table_as_2d_list(table: str) -> Tuple[List[str], List[List[str]]]:
-    rows = table.strip().split("\n")
-    column_names = [c.strip() for c in rows[0].split("|") if c != ""]
-    data = [[v.strip() for v in r.split('|') if v != ""] for r in rows[1:]]
-
-    return column_names, data
-
-
-def _read_string_table_as_dataframe(table: str) -> pd.DataFrame:
-
-    column_names, data = _read_string_table_as_2d_list(table)
-
-    return pd.DataFrame(data, columns=column_names, dtype="string")
+from .table_helpers import read_string_table_as_2d_list, read_string_table_as_dataframe
 
 
 def assert_dataframe_equals_table(table: str, actual: pd.DataFrame) -> None:
 
-    expected = _read_string_table_as_dataframe(table)
+    expected = read_string_table_as_dataframe(table)
 
     # Not concerned about data type for these tests, so just cast everything
     # to string to simplify the assertions
@@ -36,7 +23,7 @@ def assert_dataframe_equals_table(table: str, actual: pd.DataFrame) -> None:
 
 def assert_dataframe_has_columns(columns: str, actual: pd.DataFrame) -> None:
 
-    _, data = _read_string_table_as_2d_list(columns)
+    _, data = read_string_table_as_2d_list(columns)
 
     e = sorted([d[0] for d in data])
     a = sorted(list(actual.columns))
