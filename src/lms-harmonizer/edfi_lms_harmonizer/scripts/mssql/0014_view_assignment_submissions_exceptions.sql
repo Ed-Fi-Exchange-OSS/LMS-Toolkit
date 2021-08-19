2a19ab7f7dd2d14238d96883ad84ed8b91829eec
Luis Richmond <lalrg@outlook.com>
2021-08-19 16:33:57 -0600

    add validations to avoid duplicated rows

--- src/lms-harmonizer/edfi_lms_harmonizer/scripts/mssql/0014_view_assignment_submissions_exceptions.sql
+++ src/lms-harmonizer/edfi_lms_harmonizer/scripts/mssql/0014_view_assignment_submissions_exceptions.sql
@@ -7,5 +7,13 @@ CREATE VIEW lmsx.assignment_submissions_exceptions AS
     SELECT * FROM LMS.AssignmentSubmission lmsAssignmentSubmission
     WHERE NOT EXISTS(
             SELECT 1 FROM lmsx.AssignmentSubmission lmsxassignmentsubmission
-                where lmsxassignmentsubmission.AssignmentSubmissionIdentifier = lmsAssignmentSubmission.SourceSystemIdentifier
+                INNER JOIN lmsx.Assignment lmsxassginment
+					ON lmsxassginment.AssignmentIdentifier = lmsxassignmentsubmission.AssignmentIdentifier
+				INNER JOIN lms.Assignment lmsassginment
+					ON lmsxassginment.AssignmentIdentifier = lmsassginment.SourceSystemIdentifier
+				INNER JOIN edfi.Descriptor sourcesystemdescriptor
+					ON sourcesystemdescriptor.DescriptorId = lmsxassginment.LMSSourceSystemDescriptorId
+
+            WHERE lmsxassignmentsubmission.AssignmentSubmissionIdentifier = lmsAssignmentSubmission.SourceSystemIdentifier
+				AND sourcesystemdescriptor.CodeValue = lmsassginment.SourceSystem
         )
