b2b06577c34a32948c08d0be9c450078507fdeb6
Stephen A. Fuqua <stephen@safnet.com>
2022-01-11 21:08:13 -0600

    Updates for support of ODS/API v5.3

--- extension/lmsx.ps1
+++ extension/lmsx.ps1
@@ -10,8 +10,11 @@ Import-Module (Get-RepositoryResolvedPath "logistics\scripts\modules\tools\Tools
 
 $configuration = @{
     PackageName = "EdFi.Ods.Extensions.LMSX.1.0.0"
+    ## Uncomment the appropriate line below
     # For ODS/API Suite 3, version 5.2:
-    PackageVersion = "5.2.1"
+    #PackageVersion = "5.2.1"
+    # For ODS/API Suite 3, version 5.3:
+    PackageVersion = "5.3.2"
     PackageSource = "https://pkgs.dev.azure.com/ed-fi-alliance/Ed-Fi-Alliance-OSS/_packaging/EdFi/nuget/v3/index.json"
 }
 
