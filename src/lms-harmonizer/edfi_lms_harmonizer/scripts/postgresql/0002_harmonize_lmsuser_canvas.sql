41284c9942eb466d8490a57150c81debe79e1488
Stephen A. Fuqua <stephen@safnet.com>
2022-01-25 15:33:27 -0600

    Support for LMSUser

---
+++ src/lms-harmonizer/edfi_lms_harmonizer/scripts/postgresql/0002_harmonize_lmsuser_canvas.sql
@@ -0,0 +1,33 @@
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+create or replace procedure lms.harmonize_lmsuser_canvas()
+language SQL
+as $$
+
+    update
+        lms.lmsuser
+    set
+        edfistudentid = edfistudent.id
+    from
+        lms.lmsuser u
+    inner join
+        edfi.student edfistudent
+    on
+        u.sisuseridentifier = edfistudent.studentuniqueid
+    where
+        lms.lmsuser.sourcesystemidentifier = u.sourcesystemidentifier
+    and
+        lms.lmsuser.sourcesystem = u.sourcesystem
+    and
+        -- NOTE: PostgreSQL is case sensitive and this won't match "canvas".
+        -- Using "Canvas" is safe given the Canvas Extractor writes out its
+        -- CSV files with "Canvas", not "canvas"
+        u.sourcesystem = 'Canvas'
+    and
+        u.edfistudentid is null
+	and
+        u.deletedat is null;
+$$;
