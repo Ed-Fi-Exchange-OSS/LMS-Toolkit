13b944641ae3553339d71077f591e3cad87cca94
sgonzalezgap <110928073+sgonzalezgap@users.noreply.github.com>
2022-10-12 12:15:04 -0500

    [LMS-469] Extract enrollments and students from GraphQL (#276)
    * [LMS-469] Extract enrollments and students from GraphQL
    
    * [LMS-469] Change variable name has_data and revert boolean value in utils
    
    * [LMS-469] Update variable in run method from extractor

--- src/canvas-extractor/edfi_canvas_extractor/extract_graphql.py
+++ src/canvas-extractor/edfi_canvas_extractor/extract_graphql.py
@@ -7,20 +7,25 @@ import logging
 import sqlalchemy
 import sys
 
+from datetime import datetime
+from typing import Dict, List, Tuple
+
 from canvasapi import Canvas
 from canvasapi.account import Account
-from datetime import datetime
-from typing import List, Dict, Tuple
 
 from edfi_canvas_extractor.config import get_canvas_api, get_sync_db_engine
-from edfi_canvas_extractor.graphql.extractor import Extract
+from edfi_canvas_extractor.graphql.extractor import GraphQLExtractor
 from edfi_lms_extractor_lib.csv_generation.write import (
     write_sections,
+    write_section_associations,
+    write_users,
 )
 from edfi_lms_extractor_lib.helpers.decorators import catch_exceptions
 from edfi_canvas_extractor.client_graphql import (
     extract_courses,
+    extract_enrollments,
     extract_sections,
+    extract_students,
 )
 from edfi_canvas_extractor.helpers.arg_parser import MainArguments
 
@@ -39,7 +44,7 @@ def _break_execution(failing_extraction: str) -> None:
 
 @catch_exceptions
 def _get_courses(
-    gql: Extract,
+    gql: GraphQLExtractor,
     sync_db: sqlalchemy.engine.base.Engine
 ) -> None:
     logger.info("Extracting Courses from Canvas")
@@ -53,15 +58,72 @@ def _get_courses(
 
 @catch_exceptions
 def _get_sections(
-    arguments: MainArguments,
-    gql: Extract,
+    gql: GraphQLExtractor,
     sync_db: sqlalchemy.engine.base.Engine
 ) -> None:
     logger.info("Extracting Sections from Canvas")
-    (sections, udm_sections_df, all_section_ids) = extract_sections(gql, sync_db)
+    sections, udm_sections_df, all_section_ids = extract_sections(gql, sync_db)
+
+    if "sections" not in results_store:
+        results_store["sections"] = (sections, udm_sections_df, all_section_ids)
+    else:
+        sections_temp, udm_sections_df_temp, all_section_ids_temp = results_store["sections"]
+
+        sections_temp.extend(sections)
+        udm_sections_df_temp = udm_sections_df_temp.append(udm_sections_df)
+        all_section_ids_temp.extend(all_section_ids)
+
+        results_store["sections"] = (sections_temp, udm_sections_df_temp, all_section_ids_temp)
+
+
+@catch_exceptions
+def _write_sections(
+    arguments: MainArguments,
+) -> None:
+    _, udm_sections_df, _ = results_store["sections"]
+
     logger.info("Writing LMS UDM Sections to CSV file")
     write_sections(udm_sections_df, datetime.now(), arguments.output_directory)
-    results_store["sections"] = (sections, udm_sections_df, all_section_ids)
+
+
+@catch_exceptions
+def _get_students(
+    gql: GraphQLExtractor,
+    sync_db: sqlalchemy.engine.base.Engine
+) -> None:
+    logger.info("Extracting Students from Canvas")
+    (students, udm_students_df) = extract_students(gql, sync_db)
+    results_store["students"] = (students, udm_students_df)
+
+
+@catch_exceptions
+def _write_students(arguments: MainArguments) -> None:
+    (students, udm_students_df) = results_store["students"]
+
+    logger.info("Writing LMS UDM Users to CSV file")
+    write_users(udm_students_df, datetime.now(), arguments.output_directory)
+
+
+@catch_exceptions
+def _get_enrollments(
+    gql: GraphQLExtractor,
+    sync_db: sqlalchemy.engine.base.Engine
+) -> None:
+    logger.info("Extracting Enrollments from Canvas")
+    (_, _, all_section_ids) = results_store["sections"]
+    (enrollments, udm_enrollments) = extract_enrollments(gql, all_section_ids, sync_db)
+    results_store["enrollments"] = (enrollments, udm_enrollments)
+
+
+@catch_exceptions
+def _write_sections_associations(arguments: MainArguments) -> None:
+    _, _, all_section_ids = results_store["sections"]
+    _, udm_enrollments = results_store["enrollments"]
+
+    logger.info("Writing LMS UDM UserSectionAssociations to CSV files")
+    write_section_associations(
+        udm_enrollments, all_section_ids, datetime.now(), arguments.output_directory
+    )
 
 
 def run(arguments: MainArguments) -> None:
@@ -75,22 +137,39 @@ def run(arguments: MainArguments) -> None:
 
     for account in accounts:
         _id = getattr(account, "id")
-        gql = Extract()
-        gql.set_credentials(arguments.base_url, arguments.access_token)
-        gql.set_account(_id)
-        gql.set_dates(arguments.start_date, arguments.end_date)
+
+        logger.info(f"Getting data for account ID {_id}")
+
+        gql = GraphQLExtractor(
+            arguments.base_url,
+            arguments.access_token,
+            _id,
+            arguments.start_date,
+            arguments.end_date
+            )
         gql.run()
 
-    succeeded: bool = True
+        succeeded: bool = True
 
-    succeeded = _get_courses(gql, sync_db)
+        succeeded = _get_courses(gql, sync_db)
+        if not succeeded:
+            _break_execution("Courses")
 
-    if not succeeded:
-        _break_execution("Courses")
+        succeeded = _get_sections(gql, sync_db)
+        if not succeeded:
+            _break_execution("Sections")
 
-    succeeded = _get_sections(arguments, gql, sync_db)
+        succeeded = _get_students(gql, sync_db)
+        if not succeeded:
+            _break_execution("Students")
 
-    if not succeeded:
-        _break_execution("Sections")
+        succeeded = _get_enrollments(gql, sync_db)
+        if not succeeded:
+            _break_execution("Enrollments")
+
+        _write_sections_associations(arguments)
+
+    _write_sections(arguments)
+    _write_students(arguments)
 
     logger.info("Finishing Ed-Fi LMS Canvas Extractor")
---
+++ src/canvas-extractor/edfi_canvas_extractor/graphql/enrollments.py
@@ -0,0 +1,75 @@
+﻿# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
+import logging
+from typing import List
+
+from pandas import DataFrame
+import sqlalchemy
+
+from canvasapi.enrollment import Enrollment
+from edfi_lms_extractor_lib.api.resource_sync import (
+    cleanup_after_sync,
+    sync_to_db_without_cleanup,
+)
+from .canvas_helper import to_df
+
+ENROLLMENTS_RESOURCE_NAME = "Enrollments"
+
+logger = logging.getLogger(__name__)
+
+
+def enrollments_synced_as_df(
+    enrollments: List[Enrollment],
+    sync_db: sqlalchemy.engine.base.Engine,
+) -> DataFrame:
+    """
+    Fetch Enrollments API data for a range of students and return a Enrollments API DataFrame
+    with current and previously fetched data
+
+    Parameters
+    ----------
+    enrollments: List[Enrollment]
+        a list of Canvas Enrollments objects
+    sync_db: sqlalchemy.engine.base.Engine
+        an Engine instance for creating database connections
+
+    Returns
+    -------
+    DataFrame
+        a Enrollments API DataFrame with the current and previously fetched data
+    """
+    enrollments_df: DataFrame = _sync_without_cleanup(to_df(enrollments), sync_db)
+    cleanup_after_sync(ENROLLMENTS_RESOURCE_NAME, sync_db)
+
+    return enrollments_df
+
+
+def _sync_without_cleanup(
+    resource_df: DataFrame, sync_db: sqlalchemy.engine.base.Engine
+) -> DataFrame:
+    """
+    Take fetched API data and sync with database. Creates tables when necessary,
+    but ok if temporary tables are there to start. Doesn't delete temporary tables when finished.
+
+    Parameters
+    ----------
+    resource_df: DataFrame
+        a Enrollments API DataFrame with the current fetched data which
+        will be mutated, adding Hash and CreateDate/LastModifiedDate
+    sync_db: sqlalchemy.engine.base.Engine
+        an Engine instance for creating database connections
+
+    Returns
+    -------
+    DataFrame
+        a DataFrame with current fetched data and reconciled CreateDate/LastModifiedDate
+    """
+    return sync_to_db_without_cleanup(
+        resource_df=resource_df,
+        identity_columns=["id"],
+        resource_name=ENROLLMENTS_RESOURCE_NAME,
+        sync_db=sync_db,
+    )
