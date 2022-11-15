db92f2cc619244eced52bd66ca3e3a41b0b52ae5
Sergio González <sgonzalez@growthaccelerationpartners.com>
2022-11-15 13:28:36 -0600

    [LMS-488] Add autouse to fixture and removing from the class

--- src/canvas-extractor/tests/graphql/extractor/test_extractor_integration.py
+++ src/canvas-extractor/tests/graphql/extractor/test_extractor_integration.py
@@ -11,8 +11,6 @@ from pathlib import Path
 from sqlalchemy import create_engine
 
 from edfi_canvas_extractor.graphql.extractor import GraphQLExtractor
-from edfi_canvas_extractor.graphql.schema import query_builder
-
 from edfi_canvas_extractor.graphql.assignments import assignments_synced_as_df
 from edfi_canvas_extractor.graphql.courses import courses_synced_as_df
 from edfi_canvas_extractor.graphql.enrollments import enrollments_synced_as_df
@@ -21,7 +19,7 @@ from edfi_canvas_extractor.graphql.students import students_synced_as_df
 
 
 @pytest.fixture(scope="class")
-def gql_class():
+def gql():
     CANVAS_BASE_URL = os.environ['CANVAS_BASE_URL']
     CANVAS_ACCESS_TOKEN = os.environ['CANVAS_ACCESS_TOKEN']
     START_DATE = "2021-01-01"
@@ -34,16 +32,14 @@ def gql_class():
         START_DATE,
         END_DATE,
         )
+    gql.run()
+
     yield gql
 
 
-@pytest.fixture(scope="class")
-def graphql_class(gql_class):
-    query = query_builder(account_id=1)
-    data = gql_class.get_from_canvas(query)
-    gql_class.extract(data)
-
-    yield gql_class
+@pytest.fixture(autouse=True, scope="class")
+def graphql(request, gql):
+    request.cls.graphql_extractor = gql
 
 
 @pytest.fixture(scope="class")
@@ -54,125 +50,122 @@ def test_db_fixture():
     # Path(DB_FILE).unlink(missing_ok=True)
 
 
-@pytest.mark.skip
+@pytest.mark.usefixtures("graphql")
 class TestExtractorIntegration:
 
-    def test_extractor_request(self, gql_class):
-        query = query_builder(account_id=1)
-        data = gql_class.get_from_canvas(query)
+    def test_gql_data_fetched(self):
+        assert self.graphql_extractor.has_data is True  # type: ignore
 
-        assert data is not None
-
-    def test_extractor_courses(self, graphql_class):
+    def test_gql_courses_not_empty(self):
         """
         Get from the sample data
         obtain the courses info
         Check and check the return type
         """
-        courses = graphql_class.get_courses()
+        courses = self.graphql_extractor.get_courses()  # type: ignore
 
         assert courses is not None
         assert isinstance(courses, list)
 
-    def test_extractor_courses_df(self, graphql_class, test_db_fixture):
+    def test_gql_courses_df_structure(self, test_db_fixture):
         """
         Get from the sample data
         obtain the courses info
         Check the DataFrame
         """
-        courses = graphql_class.get_courses()
+        courses = self.graphql_extractor.get_courses()  # type: ignore
         courses_df = courses_synced_as_df(courses, test_db_fixture)
 
         assert courses_df is not None
         assert isinstance(courses_df, DataFrame)
 
-    def test_extractor_sections(self, graphql_class):
+    def test_gql_sections_not_empty(self):
         """
         Get from the sample data
         obtain the sections info
         Check and check the return type
         """
-        sections = graphql_class.get_sections()
+        sections = self.graphql_extractor.get_sections()  # type: ignore
 
         assert sections is not None
         assert isinstance(sections, list)
 
-    def test_extractor_sections_df(self, graphql_class, test_db_fixture):
+    def test_gql_sections_df_structure(self, test_db_fixture):
         """
         Get from the sample data
         obtain the sections info
         Check the DataFrame
         """
-        sections = graphql_class.get_sections()
+        sections = self.graphql_extractor.get_sections()  # type: ignore
         sections_df = sections_synced_as_df(sections, test_db_fixture)
 
         assert sections_df is not None
         assert isinstance(sections_df, DataFrame)
 
-    def test_extractor_students(self, graphql_class):
+    def test_gql_students_not_empty(self):
         """
         Get from the sample data
         obtain the students info
         Check and check the return type
         """
-        students = graphql_class.get_students()
+        students = self.graphql_extractor.get_students()  # type: ignore
 
         assert students is not None
         assert isinstance(students, list)
 
-    def test_students_df(self, graphql_class, test_db_fixture):
+    def test_gql_students_df_structure(self, test_db_fixture):
         """
         Get from the sample data
         obtain the students info
         Check the DataFrame
         """
-        students = graphql_class.get_students()
+        students = self.graphql_extractor.get_students()  # type: ignore
         students_df = students_synced_as_df(students, test_db_fixture)
 
         assert students_df is not None
         assert isinstance(students_df, DataFrame)
 
-    def test_extractor_enrollments(self, graphql_class):
+    def test_gql_enrollments_not_empty(self):
         """
         Get from the sample data
         obtain the enrollments info
         Check and check the return type
         """
-        enrollments = graphql_class.get_enrollments()
+        enrollments = self.graphql_extractor.get_enrollments()  # type: ignore
 
         assert enrollments is not None
         assert isinstance(enrollments, list)
 
-    def test_enrollments_df(self, graphql_class, test_db_fixture):
+    def test_gql_enrollments_df_structure(self, test_db_fixture):
         """
         Get from the sample data
         obtain the enrollments info
         Check the DataFrame
         """
-        enrollments = graphql_class.get_enrollments()
+        enrollments = self.graphql_extractor.get_enrollments()  # type: ignore
         enrollments_df = enrollments_synced_as_df(enrollments, test_db_fixture)
 
         assert enrollments_df is not None
         assert isinstance(enrollments_df, DataFrame)
 
-    def test_extractor_assignments(self, graphql_class):
+    def test_gql_assignments_not_empty(self):
         """
         Get from the sample data
         obtain the assignments info
         Check and check the return type
         """
-        assignments = graphql_class.get_assignments()
+        assignments = self.graphql_extractor.get_assignments()  # type: ignore
 
         assert assignments is not None
         assert isinstance(assignments, list)
 
-    def test_assignments_df(self, graphql_class, test_db_fixture):
+    def test_gql_assignments_df_structure(self, test_db_fixture):
         """
         Get from the sample data
         obtain the assignments info
         Check the DataFrame
         """
-        assignments = graphql_class.get_assignments()
+        assignments = self.graphql_extractor.get_assignments()  # type: ignore
         assignments_df = assignments_synced_as_df(assignments, test_db_fixture)
 
         assert assignments_df is not None
