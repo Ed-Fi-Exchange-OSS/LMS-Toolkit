62f1b2c317c5631b5b9d7e287b88890c2b666e5f
sgonzalezgap <110928073+sgonzalezgap@users.noreply.github.com>
2022-11-24 11:54:54 -0600

    [LMS-491] Add pytest markers for unit and integration tests (#288)
    * [LMS-491] Add pytest markers for unit and integration tests
    
    * [LMS-491] Moving conftest to test root folder, change markers description, removing pytest.ini

--- src/canvas-extractor/tests/graphql/unit/enrollments/test_extractor_enrollments.py
+++ src/canvas-extractor/tests/graphql/unit/enrollments/test_extractor_enrollments.py
@@ -3,11 +3,13 @@
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
 
+import pytest
 
 from pandas import DataFrame
 from edfi_canvas_extractor.graphql.enrollments import enrollments_synced_as_df
 
 
+@pytest.mark.unit
 def test_gql_enrollments_not_empty(mock_gql):
     """
     Get from the sample data
@@ -20,6 +22,7 @@ def test_gql_enrollments_not_empty(mock_gql):
     assert isinstance(enrollments, list)
 
 
+@pytest.mark.unit
 def test_gql_enrollments_df_structure(mock_gql, test_db_fixture):
     """
     Get from the sample data
