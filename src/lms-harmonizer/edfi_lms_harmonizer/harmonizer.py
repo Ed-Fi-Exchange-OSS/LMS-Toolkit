97446ea89f98ab90462816520147010fa996f3b2
Stephen A. Fuqua <stephen@safnet.com>
2025-03-07 15:33:14 -0600

    Fix type errors detected by newer mypy

--- src/lms-harmonizer/edfi_lms_harmonizer/harmonizer.py
+++ src/lms-harmonizer/edfi_lms_harmonizer/harmonizer.py
@@ -4,6 +4,7 @@
 # See the LICENSE and NOTICES files in the project root for more information.
 
 import logging
+from typing import Optional
 
 from edfi_lms_extractor_lib.helpers.decorators import catch_exceptions
 from edfi_sql_adapter.sql_adapter import Adapter, Statement
@@ -18,7 +19,7 @@ logger = logging.getLogger(__name__)
 
 
 def _generate_call_to_stored_procedure(
-    engine: str, procedure: str, namespace: str = None, source_system: str = None
+    engine: str, procedure: str, namespace: Optional[str] = None, source_system: Optional[str] = None
 ) -> str:
     if engine == DB_ENGINE.MSSQL:
         statement = f"EXEC lms.{procedure}"
